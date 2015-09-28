1f604b9ee04402356719a530266c037b1cf0a103
karsten gebbert <k@ioctl.it>
2015-09-28 14:57:02 +0200

    alias Globals for less ugly code in example

--- native/src/Iris.Web/src/Main.fs
+++ native/src/Iris.Web/src/Main.fs
@@ -12,6 +12,9 @@ open Iris.Web.VirtualDom
 [<JSEmit("""return JSON.stringify({0});""")>]
 let toString (i : obj) = ""
 
+let document = Globals.document
+let setInterval = Globals.setInterval
+
 (* __  __       _       
   |  \/  | __ _(_)_ __  
   | |\/| |/ _` | | '_ \ 
@@ -54,12 +57,12 @@ let main() =
   let tree = ref (htmlToVTree (render !count)) 
   let rootNode = ref (createElement !tree)
 
-  Globals.document.body.appendChild(!rootNode) |> ignore
+  document.body.appendChild(!rootNode) |> ignore
 
-  Globals.setInterval((fun _ ->
-                         count := (!count + 1)
-                         let newtree = htmlToVTree <| render !count
-                         let patches = diff tree newtree
-                         rootNode := patch !rootNode patches
-                         tree := newtree
-                         ), 1000)
+  setInterval((fun _ ->
+                  count := (!count + 1)
+                  let newtree = htmlToVTree <| render !count
+                  let patches = diff tree newtree
+                  rootNode := patch !rootNode patches
+                  tree := newtree
+                  ), 1000)
