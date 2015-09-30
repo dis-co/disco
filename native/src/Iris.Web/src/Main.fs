0684eb261328256141b0eb9288f98d6c129bd8ff
karsten gebbert <k@ioctl.it>
2015-09-30 14:47:44 +0200

    introducing more structure to incoming data

--- native/src/Iris.Web/src/Main.fs
+++ native/src/Iris.Web/src/Main.fs
@@ -19,6 +19,7 @@ let setInterval = Globals.setInterval
 
 let JSON = Globals.JSON
 
+type Patch = { Name : string }
 type IOBox = { Name : string }
 
 type EventType =
@@ -26,41 +27,47 @@ type EventType =
   | RemovePin
   | UpdatePin
 
-type AppEvent = { Kind : EventType; }
+type EventData =
+  | IOBoxD of IOBox
+  | PatchD of Patch
+
+type AppEvent = { Kind : EventType; Data : EventData }
 
 type AppStore () =
   let mutable pins : IOBox list = []
   let mutable listeners : (AppEvent -> unit) list = []
 
-  member this.Pins with get () = pins
+  member o.Pins with get () = pins
 
-  member this.Add (pin : IOBox) =
+  member o.Add (pin : IOBox) =
     pins <- pin :: pins
-    List.map (fun l -> l { Kind = AddPin }) listeners
+    List.map (fun l -> l { Kind = AddPin; Data = IOBoxD pin }) listeners
 
-  member this.Update (pin : IOBox) =
+  member o.Update (pin : IOBox) =
     pins <- List.map (fun p -> if p = pin then pin else p) pins
-    List.map (fun l -> l { Kind = RemovePin }) listeners
+    List.map (fun l -> l { Kind = RemovePin; Data = IOBoxD pin }) listeners
 
-  member this.Remove (pin : IOBox) =
+  member o.Remove (pin : IOBox) =
     pins <- List.filter (fun p -> p <> pin) pins
-    List.map (fun l -> l { Kind = RemovePin }) listeners
+    List.map (fun l -> l { Kind = RemovePin; Data = IOBoxD pin }) listeners
 
-  member this.AddListener (listener : AppEvent -> unit) =
+  member o.AddListener (listener : AppEvent -> unit) =
     listeners <- listener :: listeners
- 
-  member this.ClearListeners (listener : AppEvent -> unit) =
+
+  member o.ClearListeners (listener : AppEvent -> unit) =
     listeners <- []
 
 let onMsg (store : AppStore) (str : string) =
   let parsed = str.Split(':') 
   match parsed.[0] with
-    | "add"    -> store.Add { Name = parsed.[1] }    |> ignore 
-    | "remove" -> store.Remove { Name = parsed.[1] } |> ignore
-    | _        -> console.log("unknown command")
-  
+    | "add"    -> store.Add    { Name = parsed.[1] } 
+    | "remove" -> store.Remove { Name = parsed.[1] }
+    | "update" -> store.Update { Name = parsed.[1] }
+    | _        -> console.log("unknown command"); []
+  |> ignore
+
 let onClose _ = console.log("closing")
-  
+
 (* __  __       _       
   |  \/  | __ _(_)_ __  
   | |\/| |/ _` | | '_ \ 
