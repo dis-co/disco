4d44d2894831a66b7e56046e6a46ce2cb7f3045d
karsten gebbert <k@ioctl.it>
2015-11-05 12:22:12 +0100

    fixes to Mocha mappings

--- native/src/Iris.Web.Tests/Mocha.fs
+++ native/src/Iris.Web.Tests/Mocha.fs
@@ -6,37 +6,30 @@ open System
 open WebSharper
 open WebSharper.JavaScript
 
-[<ReflectedDefinition>]
+[<JavaScript>]
 module Mocha =
-
-  [<Direct " if(!$res) { throw new Error($msg) } ">]
-  let check (res : bool) (msg : string) : unit = X<unit>
-
-  [<Direct " if(!$res) { throw new Error($res) } else { ($cb)(); } ">]
-  let check_cc (res : bool) (msg : string) (cb : unit -> unit) : unit = X<unit>
-
-  [<Direct " suite($desc) ">]
-  let suite (desc : string) : unit = X<unit>
-
-  [<Direct " test($str, $t) ">]
-  let test (str : string) (t : (unit -> unit) -> unit) : unit = X<unit>
-
-  [<Direct " test($str) ">]
-  let pending (str : string) : unit = X<unit>
 
   [<Direct " throw new Error($msg) ">]
   let fail (msg : string) : unit = X<unit>
 
-  (*----------------------------------------------------------------------------*)
+  let check (result : bool) (msg : string) : unit =
+    if not result
+    then fail msg
 
-  [<Direct(""" test($str, $t) """)>]
-  let withTestImpl (str : string) (t : (unit -> unit) -> unit) : unit = X<unit>
+  let check_cc (result : bool) (msg : string) (cb : unit -> unit) : unit =
+    if not result
+    then fail msg
+    else cb ()
 
-  let withTest (name : string) (t : unit -> unit) =
-    let worker (cont : unit -> unit, econt : exn -> unit, ccont : OperationCanceledException -> unit) : unit =
-      let wrapper (cb : unit -> unit) =
-        t () // test
-        cb () // continue if successful
-        cont () // call continuation
-      withTestImpl name wrapper
-    Async.FromContinuations(worker)
+  [<Stub>]
+  [<Name "window.suite">]
+  let suite (desc : string) : unit = X<unit>
+
+  [<Stub>]
+  [<Name "window.test">]
+  let test (str : string) (t : (unit -> unit) -> unit) : unit = X<unit>
+
+  [<Stub>]
+  [<Name "window.test">]
+  let pending (str : string) : unit = X<unit>
+
