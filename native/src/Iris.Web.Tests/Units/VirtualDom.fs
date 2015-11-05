f4f8ad0919efaa664cc64a50f02a14e7cb84dd5e
karsten gebbert <k@ioctl.it>
2015-11-05 15:40:27 +0100

    pending tests for VirtulDom

--- native/src/Iris.Web.Tests/Units/VirtualDom.fs
+++ native/src/Iris.Web.Tests/Units/VirtualDom.fs
@@ -15,5 +15,13 @@ module VirtualDom =
     // 
     // Console.Log(new VTree ("h1", props, [| new VTree("Oh wtf"); new VTree("p", new VProperties(), [| new VTree("p content") |]) |]) |> createElement)
 
-    (**)
+    (*------------------------------------------------------------------------*)
     suite "Test.Units.VirtualDom"
+    (*------------------------------------------------------------------------*)
+
+    pending "should render dom elements correctly"
+    pending "should render updates to a dom tree correctly"
+    pending "should render id attribute"
+    pending "should render style attribute"
+    pending "should render class attribute"
+
