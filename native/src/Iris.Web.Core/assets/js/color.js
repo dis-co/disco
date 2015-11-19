7f3325b03c11824bbf014ebe300938220bae46c5
karsten gebbert <k@ioctl.it>
2015-11-19 14:51:13 +0100

    adding stubs for other basic iobox types

---
+++ native/src/Iris.Web.Core/assets/js/color.js
@@ -0,0 +1,62 @@
+window.IrisPlugins = window.IrisPlugins || [];
+
+(function(plugins) {
+  var h = virtualDom.h;
+
+  function trigger(ev, listeners, ctx) {
+    listeners.forEach(function(l) {
+      l.call({ event: ev }, ctx);
+    });
+  }
+
+  function sliceView(slice) {
+    return h('div', [
+      h('h3', ['Slice: ' + slice.idx]),
+      h('input', {
+        type: 'color',
+        onchange: function (ev) {
+          console.log("onchange!");
+        }
+      }, [slice.value])
+    ]);
+  }
+  
+  var ColorPlug = function () {
+    this.listeners = {};
+
+    // get current IOBox values
+    this.get = function() {
+      console.log("get called", arguments);
+      return this.slices;
+    };
+
+    // register callback on update events to UI
+    this.on = function(tag, cb) {
+      console.log("on called", arguments);
+      this.listeners[tag] = cb;
+    };
+
+    // unregister callback
+    this.off = function(tag) {
+      console.log("off called", arguments);
+      this.listeners[tag] = null;
+    };
+
+    this.dispose = function() {
+      console.log("dispose called");
+    };
+
+    // render is expected to return a VTree even for static things like canvas
+    this.render = function (iobox) {
+      return h('div', iobox.slices.map(sliceView));
+    };
+  };
+  
+  plugins.push({
+    name: "Color Plugin",
+    type: "color",
+    create: function() {
+      return new ColorPlug(arguments);
+    }
+  });
+})(window.IrisPlugins);
---
+++ native/src/Iris.Web.Core/assets/js/node.js
@@ -0,0 +1,61 @@
+window.IrisPlugins = window.IrisPlugins || [];
+
+(function(plugins) {
+  var h = virtualDom.h;
+
+  function trigger(ev, listeners, ctx) {
+    listeners.forEach(function(l) {
+      l.call({ event: ev }, ctx);
+    });
+  }
+
+  function sliceView(slice) {
+    return h('div', [
+      h('h3', ['Slice: ' + slice.idx]),
+      h('input', {
+        onchange: function (ev) {
+          console.log("onchange!");
+        }
+      }, [slice.value])
+    ]);
+  }
+  
+  var NodePlug = function () {
+    this.listeners = {};
+
+    // get current IOBox values
+    this.get = function() {
+      console.log("get called", arguments);
+      return this.slices;
+    };
+
+    // register callback on update events to UI
+    this.on = function(tag, cb) {
+      console.log("on called", arguments);
+      this.listeners[tag] = cb;
+    };
+
+    // unregister callback
+    this.off = function(tag) {
+      console.log("off called", arguments);
+      this.listeners[tag] = null;
+    };
+
+    this.dispose = function() {
+      console.log("dispose called");
+    };
+
+    // render is expected to return a VTree even for static things like canvas
+    this.render = function (iobox) {
+      return h('div', iobox.slices.map(sliceView));
+    };
+  };
+  
+  plugins.push({
+    name: "Node Plugin",
+    type: "node",
+    create: function() {
+      return new NodePlug(arguments);
+    }
+  });
+})(window.IrisPlugins);
