c3264dbb98ad1c8eae05bad0a1b6c61b8182a871
karsten gebbert <k@ioctl.it>
2015-12-02 15:19:22 +0100

    routing messages on cluster still ???

--- native/src/Iris.Service/src/Main.fs
+++ native/src/Iris.Service/src/Main.fs
@@ -7,6 +7,7 @@ open Akka
 open Akka.Actor
 open Akka.FSharp
 open Akka.Remote
+open Akka.Routing
 open Akka.Configuration
 
 open Iris.Core.Types
@@ -16,32 +17,74 @@ module Main =
 
   [<EntryPoint>]
   let main argv =
-
     let localPort  = int(Environment.GetEnvironmentVariable("LOCAL_PORT"))
     let remotePort = Environment.GetEnvironmentVariable("REMOTE_PORT")
 
     let tmpl = @"
-akka.remote.helios.tcp {
-    transport-class = ""Akka.Remote.Transport.Helios.HeliosTcpTransport, Akka.Remote""
-    transport-protocol = tcp
-    port = %port%
-    hostname = ""127.0.0.1""
+akka {
+    actor {
+        provider = ""Akka.Cluster.ClusterActorRefProvider, Akka.Cluster""
+
+        deployment = {
+            /as = {
+                router = broadcast-group
+                routees.paths = [""../../a1"", ""../../a2"", ""../../a3""]
+                cluster = {
+										enabled = on
+										allow-local-routees = on
+										use-role = broker
+                }
+            }
+        }
+    }
+
+    remote {
+        log-remote-lifecycle-events = DEBUG
+        helios.tcp {
+            port = %localport%
+            hostname = localhost
+        }
+    }
+
+    cluster {
+        seed-nodes = [
+            ""akka.tcp://iris@localhost:%localport%"",
+            ""akka.tcp://iris@localhost:%remoteport%""
+        ]
+        roles = [ ""broker"" ]
+    }
 }"
            
-    let config = ConfigurationFactory.ParseString(tmpl.Replace("%port%", localPort.ToString()))
+    let cnfstr =
+      tmpl
+        .Replace("%localport%", localPort.ToString())
+        .Replace("%remoteport%", remotePort.ToString())
+
+    let config = ConfigurationFactory.ParseString(cnfstr)
     let wsport = localPort + 1
 
     use system = ActorSystem.Create("iris", config)
-    let websockets = WebSockets.Create system wsport
 
-    let remote = system.ActorSelection("akka.tcp://iris@localhost:"+remotePort+"/user/clients")
-      
+    let remote = system.ActorOf(Props.Empty.WithRouter(FromConfig.Instance), "as")
+
+    ["a1"; "a2"; "a3"]
+    |> List.map (fun name -> spawn system name <| fun mailbox ->
+                 let rec loop () = actor {
+                   let! msg = mailbox.Receive()
+                   printfn "%s received a message: %s" name msg
+                   return! loop()
+                 }
+                 loop())
+    |> ignore
+
+    // let websockets = WebSockets.Create system wsport
+
     // let assetServer = new AssetServer("0.0.0.0", 3000)
     // assetServer.Start ()
 
     while true do
       let cmd = Console.ReadLine()
-      websockets <! WebSockets.Broadcast cmd
-      remote <! WebSockets.Broadcast "remooooty"
+      // websockets <! WebSockets.Broadcast cmd
+      remote <! cmd
 
     0
