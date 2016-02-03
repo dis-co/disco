9adf314f414a1ccc326574b929c810e999fabd1a
Karsten Gebbert <k@ioctl.it>
2016-02-03 14:42:58 +0100

    more work on Context and small CLI interpreter

--- vvvv/plugins/src/Iris.Core/src/Types/Context.fs
+++ vvvv/plugins/src/Iris.Core/src/Types/Context.fs
@@ -1,10 +1,32 @@
 namespace Iris.Core.Types
 
+open LibGit2Sharp
 
 [<AutoOpen>]
 module Context =
 
-  type Context =
-    { Project : Project option ref
-    }
+  type Context() as self =
+    [<DefaultValue>] val mutable Signature : Signature option
+    [<DefaultValue>] val mutable Project   : Project   option
 
+    do
+      self.Project <- None
+
+    member self.LoadProject(path : FilePath) : unit =
+      self.Project <- loadProject path 
+
+    member self.SaveProject(msg : string) : unit =
+      if Option.isSome self.Signature
+      then 
+        let signature = Option.get self.Signature
+        match self.Project with
+          | Some(project) -> saveProject project signature msg
+          | _ -> printfn "No project loaded."
+      else printfn "Unable to save project. No signature supplied."
+
+    member self.CreateProject(name : Name, path : FilePath) =
+      let project = createProject name
+      project.Path <- Some(path)
+      self.Project <- Some(project)
+      self.SaveProject(sprintf "Created %s" name)
+      
--- vvvv/plugins/src/Iris.Service/src/Main.fs
+++ vvvv/plugins/src/Iris.Service/src/Main.fs
@@ -3,21 +3,28 @@
 open System.Diagnostics
 open System
 open System.Threading
-
 open Nessos.FsPickler
 open Iris.Core.Types
 open Iris.Core.Config
 open Iris.Service.Types
-
+open LibGit2Sharp
 open Vsync
 
 type LookUpHandler = delegate of string -> unit
 
 module Main =
 
+  let (|Create|Load|Save|Help|) (str : string) =
+    let parsed = str.Split(' ')
+    match parsed with
+      | [| "create"; name; path |] -> Create(name, path)
+      | [| "load";         path |] -> Load(path)
+      | [| "save";         msg  |] -> Save(msg)
+      | _ -> Help
+
   [<EntryPoint>]
   let main argv =
-    printfn "starting engine.."
+    printf "starting.."
 
     let options =
       { VsyncConfig.Default with
@@ -28,21 +35,22 @@ module Main =
     
     VsyncSystem.Start()
 
-    printfn "done."
+    let signature = new Signature("Karsten Gebbert", "k@ioctl.it", new DateTimeOffset(DateTime.Now))
+    let Context = new Context()
+    Context.Signature <- Some(signature)
 
-    // let pins = new PinGroup("iris.pins")
-    // pins.group.Join()
+    printfn "..ready."
 
-    // let pin : Pin =
-    //   { Id = System.Guid.NewGuid().ToString()
-    //   ; Name = "YeahPin"
-    //   ; IOBoxes = Array.empty
-    //   }
+    while true do
+      printf "> "
+      let cmd = Console.ReadLine()
+      match cmd with
+        | Load(path)         -> Context.LoadProject(path)
+        | Save(msg)          -> Context.SaveProject(msg)
+        | Create(name, path) -> Context.CreateProject(name, path)
+        | Help               -> printfn "help requested.."
 
-    // pins.Add(pin)
-    // pins.Send(PinAction.Add, pin)
-    // pins.Dump()
 
-    VsyncSystem.WaitForever()
+    // VsyncSystem.WaitForever()
 
     0
