a9e47a17eda45edcd85f63dca3023f4a355c2742
Karsten Gebbert <k@ioctl.it>
2016-06-24 18:14:15 -0700

    adding encode/decode Serialization interface

--- dotnet/src/Iris/Iris/Core/Serialization.fs
+++ dotnet/src/Iris/Iris/Core/Serialization.fs
@@ -1,48 +1,38 @@
 namespace Iris.Core
 
-open System.IO
-open System.Text
+/// Serialization Interface 
 
-/// provides JSON serialization for communication 
 [<AutoOpen>]
-module Serialization = 
-  let toString = Encoding.ASCII.GetString
+module Serialization =
 
-  // let serialize (value : 'U) : string =
-  //   let JsonProvider = Core.Json.Provider.Create()
-  //   let encoder = JsonProvider.GetEncoder<'U>()
-  //   use writer = new StringWriter()
+  //  _____                     _
+  // | ____|_ __   ___ ___   __| | ___
+  // |  _| | '_ \ / __/ _ \ / _` |/ _ \
+  // | |___| | | | (_| (_) | (_| |  __/
+  // |_____|_| |_|\___\___/ \__,_|\___|
 
-  //   value
-  //   |> encoder.Encode
-  //   |> JsonProvider.Pack
-  //   |> WebSharper.Core.Json.Write writer
+  /// Using static constraints, we can enforce that given types must implement a
+  /// member to convert itself into a byte buffer. The compiler will throw
+  /// errors if we don't supply it with values that implement that member. This
+  /// is like using an interface, with the added benefit that this solution does
+  /// not require the interface type to be used in type signatures throughout
+  /// the program. This is much cleaner, and makes up for the ugly signature and
+  /// implementation here.
+  let inline encode< ^T when ^T : (member Flatten : unit -> byte array)> (value: ^T) : byte array = 
+    (^T : (member Flatten : unit -> byte array) value)
 
-  //   writer.ToString()
+  //  ____                     _
+  // |  _ \  ___  ___ ___   __| | ___
+  // | | | |/ _ \/ __/ _ \ / _` |/ _ \
+  // | |_| |  __/ (_| (_) | (_| |  __/
+  // |____/ \___|\___\___/ \__,_|\___|
 
-  // let serializeBytes (value : 'U) : byte[] =
-  //   let JsonProvider = Core.Json.Provider.Create()
-  //   let encoder = JsonProvider.GetEncoder<'U>()
-  //   use stream = new MemoryStream()
-  //   use writer = new StreamWriter(stream)
-
-  //   value
-  //   |> encoder.Encode
-  //   |> JsonProvider.Pack
-  //   |> WebSharper.Core.Json.Write writer
-
-  //   writer.Flush()
-  //   stream.Flush()
-  //   stream.ToArray()
-
-  // let unserializeBytes (bytes : byte[]) : 'U =
-  //   let JsonProvider = Core.Json.Provider.Create()
-  //   let decoder = JsonProvider.GetDecoder<'U>()
-
-  //   use stream = new MemoryStream(bytes)
-  //   use reader = new StreamReader(stream)
-
-  //   stream.Seek(0L, SeekOrigin.Begin) |> ignore
-
-  //   WebSharper.Core.Json.Read reader
-  //   |> decoder.Decode
+  /// Decode requires any passed type to implement a static member called
+  /// Inflate that attempts converting a passed byte buffer into the type
+  /// specifyed in the ambient code. If the conversion succeeds, the result is
+  /// a value of type ^T wrappend in Some, else None. As with `encode` the
+  /// benefits of this approach are clear. Additionally, since there is no way
+  /// to enforce the presence of a static member on a type via intefaces, this
+  /// great since it allows us to do just that.
+  let inline decode< ^T when ^T : (static member Inflate : byte array -> ^T option)> (b: byte array) : ^T option =
+    (^T : (static member Inflate : byte array -> ^T option) b)
