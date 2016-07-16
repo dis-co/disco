e93a51ae82e749e4e9f6b529b3d40d5ba45a98c7
Karsten Gebbert <k@ioctl.it>
2016-07-16 01:34:07 -0700

    maybe fix tests

--- dotnet/build.fsx
+++ dotnet/build.fsx
@@ -355,7 +355,7 @@ Target "Pallet"
    build (setParams "Debug") "src/Pallet/Pallet.fsproj"
    build (setParams "Release") "src/Pallet/Pallet.fsproj")
 
-Target "PalletTests" (buildDebug "src/Pallet.Tests/Pallet.Tests.fsproj")
+Target "PalletTests" (buildDebug ("src" @@ "Pallet.Tests" @@ "Pallet.Tests.fsproj"))
 
 Target "RunPalletTests"
   (fun _ ->
