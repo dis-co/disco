b14372c8f94996db460920ebbe9341d0f34c6417
Karsten Gebbert <k@ioctl.it>
2016-07-22 11:19:51 -0700

    locally executed tests only interest me in case of failure

--- dotnet/build.fsx
+++ dotnet/build.fsx
@@ -438,7 +438,7 @@ Target "RunWebTests" (fun _ ->
 
     match useNix with
     | true ->
-        let args = "-p /home/k/.nix-profile/bin/phantomjs -R spec tests.html"
+        let args = "-p /home/k/.nix-profile/bin/phantomjs -R min tests.html"
         ExecProcess (fun info ->
                           info.FileName <- "mocha-phantomjs"
                           info.Arguments <- args
