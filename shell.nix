ac1df96690b9c492eec834225f63bef3e0582fae
Karsten Gebbert <k@ioctl.it>
2016-11-01 16:03:27 +0100

    shell.nix derivation to build project (not working yet)

--- shell.nix
+++ shell.nix
@@ -1,6 +1,9 @@
-with import <nixpkgs> {};
+{ pkgs ? import <nixpkgs> {} }:
 
+with pkgs;
 let
+  mono = mono46;
+
   inputs = [ stdenv curl openssl phantomjs2 ];
 
   libpath="${curl.out}/lib:${openssl.out}/lib:";
@@ -11,11 +14,67 @@ let
     export PHANTOMJS_PATH=${phantomjs2}/bin/phantomjs
   '';
 
-in rec {
-  irisEnv =  stdenv.mkDerivation {
+  service = stdenv.mkDerivation {
+    name = "Iris";
+    version = "1.0";
+    phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+
+    buildInputs = [ fsharp ];
+
+    src = ./dotnet;
+
+    unpackPhase = ''
+      cp -r $src .
+      chmod -R +rwx .
+      cd $src
+      substituteInPlace ./build.sh \
+	--replace /usr/bin/env bash ${stdenv.shell}
+    '';
+
+    buildPhase = ''
+      ./build.sh DebugDocker
+    '';
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp src/Iris/bin/Debug/Iris/* $out/bin
+    '';
+  };
+
+  image = mono: dockerTools.buildImage {
+    name = "iris-image";
+    tag = "0.1";
+
+    runAsRoot = ''
+      #!${stdenv.shell}
+      export PATH=/bin:/usr/bin:/sbin:/usr/sbin:$PATH
+      ${dockerTools.shadowSetup}
+      mkdir /data
+    '';
+
+    contents = [ mono myApp ];
+
+    config = {
+      Cmd = [ "${mono}/bin/mono" "${myApp}/bin/Main.exe"];
+      ExposedPorts = {
+	"6379/tcp" = {};
+      };
+      WorkingDir = "/data";
+      Volumes = {
+	"/data" = {};
+      };
+    };
+  };
+
+  env = stdenv.mkDerivation {
     name = "irisEnv";
     buildInputs = inputs;
     libpath = libpath;
     shellHook = hook;
   };
+
+in rec {
+  irisService = service;
+  dockerImage = image;
+  irisEnv = env;
 }
