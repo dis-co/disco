01a91bf5cf71bce6651ebad333a3f51ad9b48459
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2016-11-22 11:54:56 +0100

    Add SessionStatusType

--- dotnet/src/Iris/Iris/Core/Session.fs
+++ dotnet/src/Iris/Iris/Core/Session.fs
@@ -25,6 +25,17 @@ open SharpYaml.Serialization
 //   |_|\__,_|_| |_| |_|_|  \___/|_.__// |\___|\___|\__|
 //                                   |__/
 
+type SessionStatusYaml(typ, payload) as self =
+  [<DefaultValue>] val mutable StatusType : string
+  [<DefaultValue>] val mutable Payload    : string
+
+  new () = new SessionStatusYaml(null, null)
+
+  do
+    self.StatusType <- typ
+    self.Payload    <- payload
+
+
 type SessionYaml(id, name, ip, ua) as self =
   [<DefaultValue>] val mutable Id        : string
   [<DefaultValue>] val mutable UserName  : string
@@ -47,12 +58,88 @@ type SessionYaml(id, name, ip, ua) as self =
 //  ___) |  __/\__ \__ \ | (_) | | | |
 // |____/ \___||___/___/_|\___/|_| |_|
 
+type SessionStatusType =
+  | Login
+  | Unathorized
+  | Authorized
+
+  override self.ToString() =
+    match self with
+    | Login -> "Login"
+    | Unathorized -> "Unathorized"
+    | Authorized  -> "Authorized"
+
+  static member Parse (str: string) =
+    match str with
+    | "Login" -> Login
+    | "Unathorized" -> Unathorized
+    | "Authorized"  -> Authorized
+    | _         -> failwithf "SessionStatusType: failed to parse %s" str
+
+  static member TryParse (str: string) =
+    try
+      str |> SessionStatusType.Parse |> Either.succeed
+    with
+      | exn ->
+        sprintf "Could not parse SessionStatusType: %s" exn.Message
+        |> ParseError
+        |> Either.fail
+
+  member self.ToOffset () =
+    match self with
+      | Login -> SessionStatusTypeFB.LoginFB
+      | Unathorized -> SessionStatusTypeFB.UnathorizedFB
+      | Authorized  -> SessionStatusTypeFB.AuthorizedFB
+
+  static member FromFB (fb: SessionStatusTypeFB) =
+#if FABLE_COMPILER
+    match fb with
+      | x when x = SessionStatusTypeFB.LoginFB -> Right Login
+      | x when x = SessionStatusTypeFB.UnathorizedFB -> Right Unathorized
+      | x when x = SessionStatusTypeFB.AuthorizedFB  -> Right Authorized
+      | x ->
+        sprintf "Could not parse SessionStatusType: %A" x
+        |> ParseError
+        |> Either.fail
+#else
+    match fb with
+      | SessionStatusTypeFB.LoginFB -> Right Login
+      | SessionStatusTypeFB.UnathorizedFB -> Right Unathorized
+      | SessionStatusTypeFB.AuthorizedFB  -> Right Authorized
+      | x ->
+        sprintf "Could not parse SessionStatusType: %A" x
+        |> ParseError
+        |> Either.fail
+#endif
+
+type SessionStatus =
+  { StatusType: SessionStatusType
+  ; Payload: string }
+
+  static member FromFB(fb: SessionStatusFB) : Either<IrisError, SessionStatus> =
+    SessionStatusType.FromFB fb.StatusType
+    |> Either.map (fun typ -> { StatusType = typ; Payload  = fb.Payload })
+
+  static member FromBytes(bytes: Binary.Buffer) : Either<IrisError, SessionStatus> =
+    Binary.createBuffer bytes
+    |> SessionStatusFB.GetRootAsSessionStatusFB
+    |> SessionStatus.FromFB
+
+  member self.ToOffset(builder: FlatBufferBuilder) =
+    let typ = self.StatusType.ToOffset()
+    let payload = self.Payload |> builder.CreateString
+    SessionStatusFB.StartSessionStatusFB(builder)
+    SessionStatusFB.AddStatusType(builder, typ)
+    SessionStatusFB.AddPayload(builder, payload)
+    SessionStatusFB.EndSessionStatusFB(builder)
+
+  member self.ToBytes() = Binary.buildBuffer self
+
 type Session =
   { Id: Id
-  ; UserName:  UserName
+  ; Status:  SessionStatus
   ; IpAddress: IpAddress
-  ; UserAgent: UserAgent
-  }
+  ; UserAgent: UserAgent }
 
   //  ____  _
   // | __ )(_)_ __   __ _ _ __ _   _
@@ -62,11 +149,12 @@ type Session =
   //                           |___/
 
   static member FromFB(fb: SessionFB) : Either<IrisError, Session> =
-    Either.tryWith ParseError "Session" <| fun _ ->
+    SessionStatus.FromFB fb.Status
+    |> Either.map (fun status ->
       { Id = Id fb.Id
-      ; UserName  = fb.UserName
+      ; Status  = status
       ; IpAddress = IpAddress.Parse fb.IpAddress
-      ; UserAgent = fb.UserAgent }
+      ; UserAgent = fb.UserAgent })
 
   static member FromBytes(bytes: Binary.Buffer) : Either<IrisError,Session> =
     Binary.createBuffer bytes
@@ -75,12 +163,12 @@ type Session =
 
   member self.ToOffset(builder: FlatBufferBuilder) =
     let session = self.Id |> string |> builder.CreateString
-    let name = self.UserName |> builder.CreateString
+    let status = self.Status.ToOffset(builder)
     let ip = self.IpAddress |> string |> builder.CreateString
     let ua = self.UserAgent |> string |> builder.CreateString
     SessionFB.StartSessionFB(builder)
     SessionFB.AddId(builder, session)
-    SessionFB.AddUserName(builder, name)
+    SessionFB.AddStatus(builder, status)
     SessionFB.AddIpAddress(builder, ip)
     SessionFB.AddUserAgent(builder, ua)
     SessionFB.EndSessionFB(builder)
