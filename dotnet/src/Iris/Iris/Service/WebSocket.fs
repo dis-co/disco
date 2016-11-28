5ca311ff96b4d5a8f440e70487139726cbdf3176
Karsten Gebbert <k@ioctl.it>
2016-11-28 16:17:08 +0100

    using an internal MBP and observable to subscribe to events

--- dotnet/src/Iris/Iris/Service/WebSocket.fs
+++ dotnet/src/Iris/Iris/Service/WebSocket.fs
@@ -20,17 +20,22 @@ module WebSockets =
   [<Literal>]
   let private tag = "WebSocket"
 
+  // ** WsEvent
+
+  [<NoComparison;NoEquality>]
+  type WsEvent =
+    | OnOpen    of Id
+    | OnClose   of Id
+    | OnMessage of Id * StateMachine
+    | OnError   of Id * Exception
+
   // ** Connections
 
   type private Connections = ConcurrentDictionary<Id,IWebSocketConnection>
 
-  // ** IWsServerCallbacks
+  // ** Subscriptions
 
-  type IWsServerCallbacks =
-    abstract OnOpen    : Id -> unit
-    abstract OnClose   : Id -> unit
-    abstract OnError   : Id -> Exception -> unit
-    abstract OnMessage : Id -> StateMachine -> unit
+  type private Subscriptions = ResizeArray<IObserver<WsEvent>>
 
   // ** IWsServer
 
@@ -39,6 +44,11 @@ module WebSockets =
     abstract Send         : Id -> StateMachine -> Either<IrisError,unit>
     abstract Broadcast    : StateMachine -> Either<IrisError list,unit>
     abstract BuildSession : Id -> Session -> Either<IrisError,Session>
+    abstract Subscribe    : (WsEvent -> unit) -> System.IDisposable
+
+  // ** WsEventProcessor
+
+  type private WsEventProcessor = MailboxProcessor<WsEvent>
 
   // ** getConnectionId
 
@@ -135,116 +145,6 @@ module WebSockets =
     | [ ] -> Right ()
     | _   -> Left result
 
-  // ** onOpen
-
-  /// ## onOpen
-  ///
-  /// Callback which is run when a new connection was established to a browser client. The
-  /// connections session Id gets stored in the global sessions map for later use.
-  ///
-  /// ### Signature:
-  /// - socket: IWebSocketConnection that was newly established
-  ///
-  /// Returns: unit
-  let private onOpen (id: Id)
-                     (connections: Connections)
-                     (callbacks: IWsServerCallbacks)
-                     (socket: IWebSocketConnection) () =
-    let sid = getConnectionId socket
-    connections.AddOrUpdate(sid, socket, fun _ s -> s) |> ignore
-    callbacks.OnOpen sid
-
-    sprintf "Connection added: %O" sid
-    |> Logger.debug id tag
-
-  // ** onClose
-
-  /// ## onClose
-  ///
-  /// Callback to be run when a connection was gracefully closed by the peer. The connections
-  /// session Id will be removed from the global sessions map.
-  ///
-  /// ### Signature:
-  /// - socket: IWebSocketConnection which was closed
-  ///
-  /// Returns: unit
-  let private onClose (id: Id)
-                      (connections: Connections)
-                      (callbacks: IWsServerCallbacks)
-                      (socket: IWebSocketConnection) () =
-    let sid = getConnectionId socket
-    let success, _ = connections.TryRemove(sid)
-    callbacks.OnClose sid
-
-    sprintf "Removing connection: %O - Succeeded: %b" sid success
-    |> Logger.debug id tag
-
-  // ** onMessage
-
-  /// ## onMessage
-  ///
-  /// Callback which responds to newly arriving messages on the WebSocket connections. If a handler
-  /// is registered with this server, it is invoked with the result of the decoding process of the
-  /// arrived byte buffer.
-  ///
-  /// ### Signature:
-  /// - socket: IWebSocketConnection which the new message arrived on
-  /// - msg: Binary.Buffer byte array that was received
-  ///
-  /// Returns: unit
-  let private onMessage (id: Id)
-                        (callbacks: IWsServerCallbacks)
-                        (socket: IWebSocketConnection)
-                        (msg: Binary.Buffer) =
-    /// get the Id for this connection
-    let sid = getConnectionId socket
-
-    /// decode the binary buffer as `StateMachine` command
-    let entry : Either<IrisError,StateMachine> = Binary.decode msg
-
-    /// Process the result of decoding the received message
-    match entry with
-    | Right command ->
-      /// handle the result
-      callbacks.OnMessage sid command
-
-      /// log some debugging messages
-      command
-      |> string
-      |> sprintf "command received from session %s and decoded as %s" (string sid)
-      |> Logger.debug id tag
-
-    | Left  error   ->
-      /// log the error globally
-      error
-      |> string
-      |> sprintf "command received from session %s could not be decoded: %s" (string sid)
-      |> Logger.debug id tag
-
-  // ** onError
-
-  /// ## onError
-  ///
-  /// Callback invoked when a connection was closed due to an error. Removes the corresponding
-  /// session from the global session map.
-  ///
-  /// ### Signature:
-  /// - socket: IWebSocketConnection that errored out
-  /// - exn: Exception thrown on the connection
-  ///
-  /// Returns: unit
-  let private onError (id: Id)
-                      (connections: Connections)
-                      (callbacks: IWsServerCallbacks)
-                      (socket: IWebSocketConnection)
-                      (exn: 'a when 'a :> Exception) =
-    let sid = getConnectionId socket
-    let success, _ = connections.TryRemove(sid)
-    callbacks.OnError sid exn
-
-    sprintf "Removing session %O because fo error: %s - Succeeded %b" sid exn.Message success
-    |> Logger.err id tag
-
   // ** onNewSocket
 
   /// ## onNewSocket
@@ -257,12 +157,60 @@ module WebSockets =
   /// Returns: unit
   let private onNewSocket (id: Id)
                           (connections: Connections)
-                          (callbacks: IWsServerCallbacks)
+                          (agent: WsEventProcessor)
                           (socket: IWebSocketConnection) =
-    socket.OnOpen   <- new System.Action(onOpen id connections callbacks socket)
-    socket.OnClose  <- new System.Action(onClose id connections callbacks socket)
-    socket.OnBinary <- new System.Action<Binary.Buffer>(onMessage id callbacks socket)
-    socket.OnError  <- new System.Action<exn>(onError id connections callbacks socket)
+    socket.OnOpen <- fun () ->
+      let sid = getConnectionId socket
+      connections.TryAdd(sid, socket) |> ignore
+      agent.Post(OnOpen sid)
+
+      sid
+      |> string
+      |> sprintf "New connection opened: %s"
+      |> Logger.info id tag
+
+    socket.OnClose <- fun () ->
+      let sid = getConnectionId socket
+      connections.TryRemove(sid) |> ignore
+      agent.Post(OnClose sid)
+
+      sid
+      |> string
+      |> sprintf "Connection closed: %s"
+      |> Logger.info id tag
+
+    socket.OnBinary <- fun bytes ->
+      let sid = getConnectionId socket
+      match Binary.decode bytes with
+      | Right cmd -> agent.Post(OnMessage(sid, cmd))
+      | Left err  ->
+        err
+        |> string
+        |> sprintf "Could not decode message: %s"
+        |> Logger.err id tag
+
+    socket.OnError <- fun exn ->
+      let sid = getConnectionId socket
+      connections.TryRemove(sid) |> ignore
+      agent.Post(OnError(sid, exn))
+
+      sid
+      |> string
+      |> sprintf "Error %A on websocket: %s" exn.Message
+      |> Logger.err id tag
+
+  // ** loop
+
+  let private loop (initial: Subscriptions)(inbox: WsEventProcessor) =
+    let rec act (subscriptions: Subscriptions) = async {
+        let! msg = inbox.Receive()
+        for sub in subscriptions do
+          sub.OnNext msg
+        do! act subscriptions
+      }
+    act initial
+
+  // ** WsServer
 
   //  ____        _     _ _
   // |  _ \ _   _| |__ | (_) ___
@@ -273,20 +221,36 @@ module WebSockets =
   [<RequireQualifiedAccess>]
   module WsServer =
 
-    let start (node: RaftNode) (callbacks: IWsServerCallbacks) =
+    let start (node: RaftNode) =
       either {
         let connections = new Connections()
+        let subscriptions = new Subscriptions()
+
+        let listener =
+          { new IObservable<WsEvent> with
+              member self.Subscribe(obs) =
+                lock subscriptions <| fun _ ->
+                  subscriptions.Add obs
+
+                { new IDisposable with
+                    member self.Dispose () =
+                      lock subscriptions <| fun _ ->
+                        subscriptions.Remove obs
+                        |> ignore } }
+
+        let agent = WsEventProcessor.Start(loop subscriptions)
+
         let uri = sprintf "ws://%s:%d" (string node.IpAddr) node.WsPort
 
         uri
         |> sprintf "Starting WebSocketServer on: %s"
         |> Logger.debug node.Id tag
 
-        let handler = onNewSocket node.Id connections callbacks
+        let handler = onNewSocket node.Id connections agent
         let server = new WebSocketServer(uri)
 
         try
-          server.Start(new System.Action<IWebSocketConnection>(handler))
+          server.Start(new Action<IWebSocketConnection>(handler))
 
           "WebSocketServer successfully started"
           |> Logger.debug node.Id tag
@@ -302,10 +266,19 @@ module WebSockets =
                 member self.BuildSession (id: Id) (session: Session) =
                   buildSession connections id session
 
+                member self.Subscribe (callback: WsEvent -> unit) =
+                  { new IObserver<WsEvent> with
+                      member self.OnCompleted() = ()
+                      member self.OnError(error) = ()
+                      member self.OnNext(value) = callback value
+                    }
+                  |> listener.Subscribe
+
                 member self.Dispose () =
                   for KeyValue(_, connection) in connections do
                     connection.Close()
                   connections.Clear()
+                  subscriptions.Clear()
                   dispose server }
         with
           | exn ->
