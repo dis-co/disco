1ecebaed7d5239849cc0aa7e97fec894f3287bdd
Alfonso García-Caro <alfonso.garcia-caro@hotmail.com>
2016-12-21 12:12:11 +0100

    Add project widget

--- dotnet/src/Iris/Iris/Web/React/src/PanelLeft.js
+++ dotnet/src/Iris/Iris/Web/React/src/PanelLeft.js
@@ -2,21 +2,39 @@ import React from 'react';
 import Tabs from 'muicss/lib/react/tabs';
 import Tab from 'muicss/lib/react/tab';
 import TreeView from './widgets/TreeView';
+import * as treeData from "./data/tree.js";
+import { map } from './Util';
 
-import treeData from "./data/tree.js";
+function projectDataFilter(k, v) {
+  switch (v.constructor.name) {
+    case "Id":
+      return { module: `${k}: ${v.Fields[0]}`};
+    case "LogLevel":
+      return { module: `${k}: ${v.Case}`};
+    case "FMap":
+      return {
+          module: k,
+          children: map(v, (kv, i) => treeData.object2tree(kv[1], "Member" + i, projectDataFilter))
+      }
+  }
+}
 
-export default function PanelLeft(props) { return (
-  <Tabs id="panel-left" style={{width: props.width}}>
-    <Tab label="PROJECT" >
-      <TreeView data={treeData} />
-    </Tab>
-    <Tab label="VIEWS" >
-      <TreeView data={treeData} />
-    </Tab>
-    <Tab label="LIBRARY" >
-      <div>
-        <p>Id excepteur cupidatat proident fugiat.</p>
-      </div>
-    </Tab>
-  </Tabs>
-)}
+
+export default function PanelLeft(props) {
+  let projectData = props.info.state.Project || {};
+  return (
+    <Tabs id="panel-left" style={{width: props.width}}>
+      <Tab label="PROJECT" >
+        <TreeView data={treeData.object2tree(projectData, "Project", projectDataFilter)} />
+      </Tab>
+      <Tab label="VIEWS" >
+        <TreeView data={treeData.sample} editable={true} />
+      </Tab>
+      <Tab label="LIBRARY" >
+        <div>
+          <p>Id excepteur cupidatat proident fugiat.</p>
+        </div>
+      </Tab>
+    </Tabs>
+  )
+}
