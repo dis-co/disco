0a802d37082c1209d345f4107b210318fae1b053
Karsten Gebbert <k@ioctl.it>
2017-01-19 13:19:54 +0100

    adding a Role to IrisClient + Serialization

--- dotnet/src/Iris/Iris/Client/Interfaces.fs
+++ dotnet/src/Iris/Iris/Client/Interfaces.fs
@@ -8,6 +8,24 @@ open System.Collections.Concurrent
 open FlatBuffers
 open Iris.Serialization.Api
 
+// * Role
+
+[<RequireQualifiedAccess>]
+type Role =
+  | Renderer
+
+  member role.ToOffset(builder: FlatBufferBuilder) =
+    match role with
+    | Renderer -> RoleFB.RendererFB
+
+  static member FromFB(fb: RoleFB) =
+    match fb with
+    | RoleFB.RendererFB -> Either.succeed Renderer
+    | x ->
+      sprintf "Unknown RoleFB value: %A" x
+      |> Error.asClientError "Role.FromFB"
+      |> Either.fail
+
 // * IrisClient
 
 //  ___      _      ____ _ _            _
@@ -18,21 +36,35 @@ open Iris.Serialization.Api
 
 type IrisClient =
   { Id: Id
-    Name: string }
+    Name: string
+    Role: Role
+    IpAddress: IpAddress
+    Port: Port }
 
   member client.ToOffset(builder: FlatBufferBuilder) =
     let id = builder.CreateString (string client.Id)
     let name = builder.CreateString client.Name
+    let ip = builder.CreateString (string client.IpAddress)
+    let role = client.Role.ToOffset(builder)
 
     IrisClientFB.StartIrisClientFB(builder)
     IrisClientFB.AddId(builder, id)
     IrisClientFB.AddName(builder, name)
+    IrisClientFB.AddRole(builder, role)
+    IrisClientFB.AddIpAddress(builder, ip)
+    IrisClientFB.AddPort(builder, uint32 client.Port)
     IrisClientFB.EndIrisClientFB(builder)
 
   static member FromFB(fb: IrisClientFB) =
-    { Id = Id fb.Id
-      Name = fb.Name }
-    |> Either.succeed
+    either {
+      let! role = Role.FromFB fb.Role
+      let! ip = IpAddress.TryParse fb.IpAddress
+      return { Id = Id fb.Id
+               Name = fb.Name
+               IpAddress = ip
+               Port = uint16 fb.Port
+               Role = role }
+    }
 
 // * IrisServer
 
@@ -59,6 +91,7 @@ type ClientEvent =
   | Registered
   | UnRegistered
   | Update
+
 
 // * IApiClient
 
