eec074d1160c29645438792fc2fb810c3c38ded7
Karsten Gebbert <k@ioctl.it>
2017-01-23 11:16:41 +0100

    fix nix environment for libgit2 (zlib dependency)

--- dotnet/src/Iris/assets/nix/runservice.nix
+++ dotnet/src/Iris/assets/nix/runservice.nix
@@ -3,8 +3,8 @@ with import <nixpkgs> {};
 {
   fsiEnv =  stdenv.mkDerivation {
     name = "fsiEnv";
-    buildInputs = [ stdenv curl openssl ];
-    libpath="${curl.out}/lib:${openssl.out}/lib:";
+    buildInputs = [ stdenv curl zlib openssl ];
+    libpath="${curl.out}/lib:${openssl.out}/lib:${zlib.out}/lib";
     shellHook = ''
       export LD_LIBRARY_PATH="$libpath":$LD_LIBRARY_PATH
       export IRIS_NODE_ID=`uuidgen`
