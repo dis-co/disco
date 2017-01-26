4b1af9c72a21e6edf73498c101945eef660ec814
Karsten Gebbert <k@ioctl.it>
2017-01-26 11:41:37 +0100

    working F# based GraphApi node

--- dotnet/src/Iris/Iris/Nodes/GraphApi.fs
+++ dotnet/src/Iris/Iris/Nodes/GraphApi.fs
@@ -1,6 +1,25 @@
-namespace Iris.Nodes
+namespace VVVV.Nodes
 
-type GraphApi =
+open System
+open System.ComponentModel.Composition
+open VVVV.PluginInterfaces.V1
+open VVVV.PluginInterfaces.V2
+open VVVV.Utils.VColor
+open VVVV.Utils.VMath
+open VVVV.Core.Logging
 
-  member self.Evaluate (spreadMax : int) =
-    printfn "current spread count is: %d" spreadMax
+[<PluginInfo(Name = "GraphApi", Category = "Awesome", Version = "0.0.1", Help = "RTFM")>]
+type GraphApi() =
+
+  [<Input("Input", DefaultValue = 1.0);DefaultValue>]
+  val mutable FInput : ISpread<double>
+
+  [<Output("Output");DefaultValue>]
+  val mutable FOutput : ISpread<double>
+
+  [<Import();DefaultValue>]
+  val mutable FLogger : ILogger
+
+  interface IPluginEvaluate with
+    member self.Evaluate (spreadMax : int) : unit =
+      self.FLogger.Log(LogType.Debug, "hi tty!");
