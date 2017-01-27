1961befa760c2ef9b6ef36afb365485408b9f9de
Karsten Gebbert <k@ioctl.it>
2017-01-27 10:39:50 +0100

    restructure Nodes; PluginState in Iris node and dispose fix

--- dotnet/src/Iris/Iris/Nodes/GraphApi.fs
+++ dotnet/src/Iris/Iris/Nodes/GraphApi.fs
@@ -1,25 +1,145 @@
-namespace VVVV.Nodes
+namespace Iris.Nodes
 
 open System
-open System.ComponentModel.Composition
 open VVVV.PluginInterfaces.V1
 open VVVV.PluginInterfaces.V2
-open VVVV.Utils.VColor
-open VVVV.Utils.VMath
 open VVVV.Core.Logging
+open Iris.Core
+open Iris.Client
 
-[<PluginInfo(Name = "GraphApi", Category = "Awesome", Version = "0.0.1", Help = "RTFM")>]
-type GraphApi() =
+[<RequireQualifiedAccess>]
+module GraphApi =
 
-  [<Input("Input", DefaultValue = 1.0);DefaultValue>]
-  val mutable FInput : ISpread<double>
+  type PluginState =
+    { Frame: uint64
+      Initialized: bool
+      Status: ServiceStatus
+      ApiClient: IApiClient
+      V1Host: IPluginHost
+      V2Host: IHDEHost
+      Logger: ILogger
+      InServer: IDiffSpread<string>
+      InPort: IDiffSpread<uint16>
+      InDebug: IDiffSpread<bool>
+      OutState: ISpread<State>
+      OutConnected: ISpread<bool>
+      Disposables: IDisposable list }
 
-  [<Output("Output");DefaultValue>]
-  val mutable FOutput : ISpread<double>
+    static member Create () =
+      { Frame = 0UL
+        Initialized = false
+        Status = ServiceStatus.Starting
+        ApiClient = Unchecked.defaultof<IApiClient>
+        V1Host = Unchecked.defaultof<IPluginHost>
+        V2Host = Unchecked.defaultof<IHDEHost>
+        Logger = Unchecked.defaultof<ILogger>
+        InServer = Unchecked.defaultof<IDiffSpread<string>>
+        InPort = Unchecked.defaultof<IDiffSpread<uint16>>
+        InDebug = Unchecked.defaultof<IDiffSpread<bool>>
+        OutState = Unchecked.defaultof<ISpread<State>>
+        OutConnected = Unchecked.defaultof<ISpread<bool>>
+        Disposables = List.empty }
 
-  [<Import();DefaultValue>]
-  val mutable FLogger : ILogger
+    interface IDisposable with
+      member self.Dispose() =
+        List.iter dispose self.Disposables // first dispose the logger to prevent the logger from
+        dispose self.ApiClient             // causing a VVVV crash. Then dispose the rest..
 
-  interface IPluginEvaluate with
-    member self.Evaluate (spreadMax : int) : unit =
-      self.FLogger.Log(LogType.Debug, "hi tty!");
+  let log (state: PluginState) (level: LogType) (msg: string) =
+    try
+      state.Logger.Log(level, msg)
+    with
+      | _ -> () // gulp
+
+  let debug (state: PluginState) (msg: string) =
+    log state LogType.Debug msg
+
+  let error (state: PluginState) (msg: string) =
+    log state LogType.Error msg
+
+  // ------------  Call Graph (for Bangs)  -------------------
+  //
+  // Evaluate
+  //    |
+  //    Process (update our world)
+  //    |  |
+  //    |  CallCue &&  UpdatePin
+  //    |      |
+  //    |      pin.Update (either values, or entire pin)
+  //    |      |
+  //    |      MkQueueJob value
+  //    |
+  //    Tick  (now flush it to vvvv)
+  //    |  |
+  //    |  VVVVGraph.FrameCount <= CurrentFrame
+  //    |  |
+  //    |  ProcessGraphWrites
+  //    |         |
+  //    |         IPin2.Spread = "|val|"
+  //    |         |
+  //    |         MkQueueJob value (Reset with current frame + 1)
+  //    |
+  //    Cleanup
+  //
+
+  let initialize (state: PluginState) =
+    if not state.Initialized then
+      let me =
+        let ip =
+          match Network.getIpAddress () with
+          | Some ip -> IpAddress.ofIPAddress ip
+          | None -> IPv4Address "127.0.0.1"
+
+        { Id = Id.Create ()
+          Name = "Vvvv GraphApi Client"
+          Role = Role.Renderer
+          Status = ServiceStatus.Starting
+          IpAddress = IPv4Address "192.168.2.105"
+          Port = 10001us }
+
+      let server =
+        let ip =
+          match state.InServer.[0] with
+          | null ->  IPv4Address "127.0.0.1"
+          | ip -> IPv4Address ip
+
+        { Id = Id.Create ()
+          Port = 10000us
+          Name = "iris.exe"
+          IpAddress = IPv4Address "192.168.2.108" }
+
+      try
+        let obs = Logger.subscribe (string >> debug state)
+
+        let result =
+          either {
+            let! client = ApiClient.create server me
+            do! client.Start()
+            return client
+          }
+        match result with
+        | Right client ->
+          debug state "successfully started ApiClient"
+          { state with
+              Initialized = true
+              ApiClient = client
+              Disposables = [ obs ] }
+        | Left error ->
+          debug state (sprintf "Error starting ApiClient: %A" error)
+          state
+      with
+        | exn ->
+          debug state (sprintf "Error starting ApiClient: %A" exn.Message)
+          debug state exn.StackTrace
+          state
+    else
+      state
+
+  let processor (state: PluginState) =
+    debug state (sprintf "Frame: %d" state.Frame)
+    { state with Frame = state.Frame + 1UL }
+
+  let evaluate (state: PluginState) (spreadMax: int) =
+    state
+    |> initialize
+    |> processor
---
+++ dotnet/src/Iris/Iris/Nodes/Nodes.fs
@@ -0,0 +1,95 @@
+namespace VVVV.Nodes
+
+open System
+open System.ComponentModel.Composition
+open VVVV.PluginInterfaces.V1
+open VVVV.PluginInterfaces.V2
+open VVVV.Utils.VColor
+open VVVV.Utils.VMath
+open VVVV.Core.Logging
+open Iris.Core
+open Iris.Nodes
+
+[<PluginInfo(Name="Iris", Category="Iris", AutoEvaluate=true)>]
+type IrisVvvvClient() =
+
+  //  ___                            _
+  // |_ _|_ __ ___  _ __   ___  _ __| |_ ___
+  //  | || '_ ` _ \| '_ \ / _ \| '__| __/ __|
+  //  | || | | | | | |_) | (_) | |  | |_\__ \
+  // |___|_| |_| |_| .__/ \___/|_|   \__|___/
+  //               |_|
+
+  [<Import();DefaultValue>]
+  val mutable V1Host: IPluginHost
+
+  [<Import();DefaultValue>]
+  val mutable V2Host: IHDEHost
+
+  [<Import();DefaultValue>]
+  val mutable FLogger: ILogger
+
+  //  ___                   _
+  // |_ _|_ __  _ __  _   _| |_ ___
+  //  | || '_ \| '_ \| | | | __/ __|
+  //  | || | | | |_) | |_| | |_\__ \
+  // |___|_| |_| .__/ \__,_|\__|___/
+  //           |_|
+
+  [<DefaultValue>]
+  [<Input("Server", IsSingle = true)>]
+  val mutable InServer: IDiffSpread<string>
+
+  [<DefaultValue>]
+  [<Input("Port", IsSingle = true)>]
+  val mutable InPort: IDiffSpread<uint16>
+
+  [<DefaultValue>]
+  [<Input("Debug", IsSingle = true, DefaultValue = 0.0)>]
+  val mutable InDebug: IDiffSpread<bool>
+
+  //   ___        _               _
+  //  / _ \ _   _| |_ _ __  _   _| |_ ___
+  // | | | | | | | __| '_ \| | | | __/ __|
+  // | |_| | |_| | |_| |_) | |_| | |_\__ \
+  //  \___/ \__,_|\__| .__/ \__,_|\__|___/
+  //                 |_|
+
+  [<DefaultValue>]
+  [<Output("State")>]
+  val mutable OutState: ISpread<State>
+
+  [<DefaultValue>]
+  [<Output("Connected", IsSingle = true, DefaultValue = 0.0)>]
+  val mutable OutConnected: ISpread<bool>
+
+  //  _                    _
+  // | |    ___   ___ __ _| |
+  // | |   / _ \ / __/ _` | |
+  // | |__| (_) | (_| (_| | |
+  // |_____\___/ \___\__,_|_|
+
+  let mutable initialized = false
+  let mutable state = Unchecked.defaultof<GraphApi.PluginState>
+
+  interface IPluginEvaluate with
+    member self.Evaluate (spreadMax: int) : unit =
+      if not initialized then
+        let state' =
+          { GraphApi.PluginState.Create() with
+              V1Host = self.V1Host
+              V2Host = self.V2Host
+              Logger = self.FLogger
+              InServer = self.InServer
+              InPort = self.InPort
+              InDebug = self.InDebug
+              OutState = self.OutState
+              OutConnected = self.OutConnected }
+        state <- state'
+        initialized <- true
+
+      state <- GraphApi.evaluate state spreadMax
+
+  interface IDisposable with
+    member self.Dispose() =
+      dispose state
