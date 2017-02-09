0ed014565e2c1abff0c5c796847e27df4a22e3fc
Karsten Gebbert <k@ioctl.it>
2017-02-09 17:14:49 +0100

    added a Project Node

--- dotnet/src/Iris/Iris/Nodes/Nodes.fs
+++ dotnet/src/Iris/Iris/Nodes/Nodes.fs
@@ -277,9 +277,6 @@ type State() =
   [<Output("Clients")>]
   val mutable OutClients: ISpread<IrisClient>
 
-  let mutable initialized = false
-  let mutable state = Unchecked.defaultof<GraphApi.PluginState>
-
   interface IPluginEvaluate with
     member self.Evaluate (spreadMax: int) : unit =
       if self.InUpdate.[0] && not (Util.isNull self.InState.[0]) then
@@ -316,3 +313,65 @@ type State() =
         self.OutSessions.AssignFrom sessions
         self.OutUsers.AssignFrom users
         self.OutClients.AssignFrom clients
+
+//  ____            _           _
+// |  _ \ _ __ ___ (_) ___  ___| |_
+// | |_) | '__/ _ \| |/ _ \/ __| __|
+// |  __/| | | (_) | |  __/ (__| |_
+// |_|   |_|  \___// |\___|\___|\__|
+//               |__/
+
+[<PluginInfo(Name="Project", Category="Iris", AutoEvaluate=true)>]
+type Project() =
+
+  [<Import();DefaultValue>]
+  val mutable Logger: ILogger
+
+  [<DefaultValue>]
+  [<Input("Project", IsSingle = true)>]
+  val mutable InProject: ISpread<IrisProject>
+
+  [<DefaultValue>]
+  [<Input("Update", IsSingle = true, IsBang = true)>]
+  val mutable InUpdate: ISpread<bool>
+
+  [<DefaultValue>]
+  [<Output("Id", IsSingle = true)>]
+  val mutable OutId: ISpread<string>
+
+  [<DefaultValue>]
+  [<Output("Name", IsSingle = true)>]
+  val mutable OutName: ISpread<string>
+
+  [<DefaultValue>]
+  [<Output("Path", IsSingle = true)>]
+  val mutable OutPath: ISpread<string>
+
+  [<DefaultValue>]
+  [<Output("CreatedOn", IsSingle = true)>]
+  val mutable OutCreatedOn: ISpread<string>
+
+  [<DefaultValue>]
+  [<Output("LastSaved", IsSingle = true)>]
+  val mutable OutLastSaved: ISpread<string>
+
+  [<DefaultValue>]
+  [<Output("Config", IsSingle = true)>]
+  val mutable OutConfig: ISpread<IrisConfig>
+
+  let mutable initialized = false
+
+  interface IPluginEvaluate with
+    member self.Evaluate (spreadMax: int) : unit =
+      if self.InUpdate.[0] && not (Util.isNull self.InProject.[0]) then
+        let project = self.InProject.[0]
+        let lastSaved =
+          match project.LastSaved with
+          | Some str -> str
+          | None -> ""
+        self.OutId.[0] <- string project.Id
+        self.OutName.[0] <- project.Name
+        self.OutPath.[0] <- project.Path
+        self.OutCreatedOn.[0] <- sprintf "%A" project.CreatedOn
+        self.OutLastSaved.[0] <- lastSaved
+        self.OutConfig.[0] <- project.Config
