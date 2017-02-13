8d983732e9368e2aeaef773112160547446be022
Karsten Gebbert <k@ioctl.it>
2017-02-13 13:28:09 +0100

    move mock data to state node for now

--- dotnet/src/Iris/Iris/Nodes/Nodes/IrisNode.fs
+++ dotnet/src/Iris/Iris/Nodes/Nodes/IrisNode.fs
@@ -11,147 +11,6 @@ open Iris.Raft
 open Iris.Core
 open Iris.Nodes
 
-
-[<AutoOpen>]
-module MockData =
-  let rand = new Random()
-
-  let rndstr() =
-    Id.Create()
-    |> string
-
-  let mkTags () =
-    [| for n in 0 .. rand.Next(1,20) do
-        let guid = Guid.NewGuid()
-        yield guid.ToString() |]
-
-  let mk() = Id.Create()
-
-  let mkPin() =
-    Pin.Toggle(mk(), rndstr(), mk(), mkTags(), [|{ Index = 0u; Value = true }|])
-
-  let mkPins () =
-    let props = [| { Key = "one"; Value = "two" }; { Key = "three"; Value = "four"} |]
-    let selected = props.[0]
-    let rgba = RGBA { Red = 255uy; Blue = 255uy; Green = 255uy; Alpha = 255uy }
-    let hsla = HSLA { Hue = 255uy; Saturation = 255uy; Lightness = 255uy; Alpha = 255uy }
-    [| Pin.Bang      (mk(), rndstr(), mk(), mkTags(), [|{ Index = 0u; Value = true    }|])
-    ; Pin.Toggle    (mk(), rndstr(), mk(), mkTags(), [|{ Index = 0u; Value = true    }|])
-    ; Pin.String    (mk(), rndstr(), mk(), mkTags(), [|{ Index = 0u; Value = rndstr()   }|])
-    ; Pin.MultiLine (mk(), rndstr(), mk(), mkTags(), [|{ Index = 0u; Value = rndstr()   }|])
-    ; Pin.FileName  (mk(), rndstr(), mk(), mkTags(), rndstr(), [|{ Index = 0u; Value = rndstr() }|])
-    ; Pin.Directory (mk(), rndstr(), mk(), mkTags(), rndstr(), [|{ Index = 0u; Value = rndstr() }|])
-    ; Pin.Url       (mk(), rndstr(), mk(), mkTags(), [|{ Index = 0u; Value = rndstr()  }|])
-    ; Pin.IP        (mk(), rndstr(), mk(), mkTags(), [|{ Index = 0u; Value = rndstr()  }|])
-    ; Pin.Float     (mk(), rndstr(), mk(), mkTags(), [|{ Index = 0u; Value = 3.0    }|])
-    ; Pin.Double    (mk(), rndstr(), mk(), mkTags(), [|{ Index = 0u; Value = double 3.0 }|])
-    ; Pin.Bytes     (mk(), rndstr(), mk(), mkTags(), [|{ Index = 0u; Value = [| 2uy; 9uy |] }|])
-    ; Pin.Color     (mk(), rndstr(), mk(), mkTags(), [|{ Index = 0u; Value = rgba }|])
-    ; Pin.Color     (mk(), rndstr(), mk(), mkTags(), [|{ Index = 0u; Value = hsla }|])
-    ; Pin.Enum      (mk(), rndstr(), mk(), mkTags(), props, [|{ Index = 0u; Value = selected }|])
-    |]
-
-  let mkUser () =
-    { Id = Id.Create()
-      UserName = rndstr()
-      FirstName = rndstr()
-      LastName = rndstr()
-      Email =  rndstr()
-      Password = rndstr()
-      Salt = rndstr()
-      Joined = System.DateTime.Now
-      Created = System.DateTime.Now }
-
-  let mkUsers () =
-    [| for n in 0 .. rand.Next(1,20) do
-        yield mkUser() |]
-
-  let mkCue () : Cue =
-    { Id = Id.Create(); Name = rndstr(); Pins = mkPins() }
-
-  let mkCues () =
-    [| for n in 0 .. rand.Next(1,20) do
-        yield mkCue() |]
-
-  let mkPatch () : Iris.Core.Patch =
-    let pins =
-      mkPins ()
-      |> Array.map toPair
-      |> Map.ofArray
-
-    { Id = Id.Create()
-      Name = rndstr()
-      Pins = pins }
-
-  let mkPatches () : Iris.Core.Patch array =
-    [| for n in 0 .. rand.Next(1,20) do
-        yield mkPatch() |]
-
-  let mkCueList () : CueList =
-    { Id = Id.Create(); Name = "Patch 3"; Cues = mkCues() }
-
-  let mkCueLists () =
-    [| for n in 0 .. rand.Next(1,20) do
-        yield mkCueList() |]
-
-  let mkMember () = Id.Create() |> Member.create
-
-  let mkMembers () =
-    [| for _ in 0 .. rand.Next(1, 6) do
-        yield mkMember () |]
-
-  let mkSession () =
-    { Id = Id.Create()
-      IpAddress = IPv4Address "127.0.0.1"
-      UserAgent = "Oh my goodness" }
-
-  let mkSessions () =
-    [| for n in 0 .. rand.Next(1,20) do
-        yield mkSession() |]
-
-  let mkProject () =
-    let machine =
-      { MachineId = Id.Create()
-        HostName  = Network.getHostName()
-        WorkSpace = "C:/dev/null"
-        WebIP     = "127.0.0.1"
-        WebPort   = 8080us
-        Version   = System.Version(1,0,0) }
-    { Id        = Id.Create()
-    ; Name      = rndstr()
-    ; Path      = "C:/dev/null"
-    ; CreatedOn = Time.createTimestamp()
-    ; LastSaved = Some (Time.createTimestamp ())
-    ; Copyright = None
-    ; Author    = None
-    ; Config    = Config.create (rndstr()) machine  }
-
-  let mkClient () : IrisClient =
-    { Id = Id.Create ()
-      Name = "Nice client"
-      Role = Role.Renderer
-      Status = ServiceStatus.Running
-      IpAddress = IPv4Address "127.0.0.1"
-      Port = 8921us }
-
-  let mkClients () =
-    [| for n in 0 .. rand.Next(1,20) do
-        yield mkClient() |]
-
-  let inline asMap arr =
-    arr
-    |> Array.map toPair
-    |> Map.ofArray
-
-  let mkState path : State =
-    { Project  = mkProject ()
-      Patches  = mkPatches () |> asMap
-      Cues     = mkCues    () |> asMap
-      CueLists = mkCueLists() |> asMap
-      Sessions = mkSessions() |> asMap
-      Users    = mkUsers   () |> asMap
-      Clients  = mkClients () |> asMap }
-
 //  ___      _
 // |_ _|_ __(_)___
 //  | || '__| / __|
@@ -199,15 +58,10 @@ type IrisNode() =
   val mutable OutStatus: ISpread<string>
 
   let mutable initialized = false
-  let mutable banged = false
   let mutable state = Unchecked.defaultof<GraphApi.PluginState>
 
   interface IPluginEvaluate with
     member self.Evaluate (spreadMax: int) : unit =
-      if banged then
-        banged <- false
-        self.OutUpdate.[0] <- false
-
       if not initialized then
         let state' =
           { GraphApi.PluginState.Create() with
@@ -222,10 +76,6 @@ type IrisNode() =
               OutStatus = self.OutStatus }
         state <- state'
         initialized <- true
-
-        self.OutState.[0] <- mkState()
-        self.OutUpdate.[0] <- true
-        banged <- true
 
       state <- GraphApi.evaluate state spreadMax
 
--- dotnet/src/Iris/Iris/Nodes/Nodes/StateNode.fs
+++ dotnet/src/Iris/Iris/Nodes/Nodes/StateNode.fs
@@ -11,6 +11,257 @@ open Iris.Raft
 open Iris.Core
 open Iris.Nodes
 
+[<AutoOpen>]
+module MockData =
+  let rand = new Random()
+
+  let rndstr() =
+    Id.Create()
+    |> string
+
+  let mkTags () =
+    [| for n in 0 .. rand.Next(1,20) do
+        let guid = Guid.NewGuid()
+        yield guid.ToString() |]
+
+  let mk() = Id.Create()
+
+  let mkPin() =
+    Pin.Toggle(mk(), rndstr(), mk(), mkTags(), [|{ Index = 0u; Value = true }|])
+
+  let mkPins () =
+    let props = [| { Key = "one"; Value = "two" }; { Key = "three"; Value = "four"} |]
+    let selected = props.[0]
+    let rgba = RGBA { Red = 255uy; Blue = 255uy; Green = 255uy; Alpha = 255uy }
+    let hsla = HSLA { Hue = 255uy; Saturation = 255uy; Lightness = 255uy; Alpha = 255uy }
+    [| Pin.Bang      (mk(), rndstr(), mk(), mkTags(), [|{ Index = 0u; Value = true    }|])
+    ; Pin.Toggle    (mk(), rndstr(), mk(), mkTags(), [|{ Index = 0u; Value = true    }|])
+    ; Pin.String    (mk(), rndstr(), mk(), mkTags(), [|{ Index = 0u; Value = rndstr()   }|])
+    ; Pin.MultiLine (mk(), rndstr(), mk(), mkTags(), [|{ Index = 0u; Value = rndstr()   }|])
+    ; Pin.FileName  (mk(), rndstr(), mk(), mkTags(), rndstr(), [|{ Index = 0u; Value = rndstr() }|])
+    ; Pin.Directory (mk(), rndstr(), mk(), mkTags(), rndstr(), [|{ Index = 0u; Value = rndstr() }|])
+    ; Pin.Url       (mk(), rndstr(), mk(), mkTags(), [|{ Index = 0u; Value = rndstr()  }|])
+    ; Pin.IP        (mk(), rndstr(), mk(), mkTags(), [|{ Index = 0u; Value = rndstr()  }|])
+    ; Pin.Float     (mk(), rndstr(), mk(), mkTags(), [|{ Index = 0u; Value = 3.0    }|])
+    ; Pin.Double    (mk(), rndstr(), mk(), mkTags(), [|{ Index = 0u; Value = double 3.0 }|])
+    ; Pin.Bytes     (mk(), rndstr(), mk(), mkTags(), [|{ Index = 0u; Value = [| 2uy; 9uy |] }|])
+    ; Pin.Color     (mk(), rndstr(), mk(), mkTags(), [|{ Index = 0u; Value = rgba }|])
+    ; Pin.Color     (mk(), rndstr(), mk(), mkTags(), [|{ Index = 0u; Value = hsla }|])
+    ; Pin.Enum      (mk(), rndstr(), mk(), mkTags(), props, [|{ Index = 0u; Value = selected }|])
+    |]
+
+  let mkUser () =
+    { Id = Id.Create()
+      UserName = rndstr()
+      FirstName = rndstr()
+      LastName = rndstr()
+      Email =  rndstr()
+      Password = rndstr()
+      Salt = rndstr()
+      Joined = System.DateTime.Now
+      Created = System.DateTime.Now }
+
+  let mkUsers () =
+    [| for n in 0 .. rand.Next(1,20) do
+        yield mkUser() |]
+
+  let mkCue () : Cue =
+    { Id = Id.Create(); Name = rndstr(); Pins = mkPins() }
+
+  let mkCues () =
+    [| for n in 0 .. rand.Next(1,20) do
+        yield mkCue() |]
+
+  let mkPatch () : Iris.Core.Patch =
+    let pins =
+      mkPins ()
+      |> Array.map toPair
+      |> Map.ofArray
+
+    { Id = Id.Create()
+      Name = rndstr()
+      Pins = pins }
+
+  let mkPatches () : Iris.Core.Patch array =
+    [| for n in 0 .. rand.Next(1,20) do
+        yield mkPatch() |]
+
+  let mkCueList () : CueList =
+    { Id = Id.Create(); Name = "Patch 3"; Cues = mkCues() }
+
+  let mkCueLists () =
+    [| for n in 0 .. rand.Next(1,20) do
+        yield mkCueList() |]
+
+  let mkMember () = Id.Create() |> Member.create
+
+  let mkMembers () =
+    [| for _ in 0 .. rand.Next(1, 6) do
+        yield mkMember () |]
+
+  let mkSession () =
+    { Id = Id.Create()
+      IpAddress = IPv4Address "127.0.0.1"
+      UserAgent = "Oh my goodness" }
+
+  let mkSessions () =
+    [| for n in 0 .. rand.Next(1,20) do
+        yield mkSession() |]
+
+  let mkMachine () =
+    { MachineId = Id.Create()
+      HostName  = Network.getHostName()
+      WorkSpace = "C:/dev/null"
+      WebIP     = "127.0.0.1"
+      WebPort   = 8080us
+      Version   = System.Version(1,0,0) }
+
+  let mkVvvv () =
+    let exes =
+      [| for n in 0 .. (rand.Next(2,8)) do
+          yield { Executable = rndstr()
+                  Version = rndstr()
+                  Required = (n % 2) = 0 } |]
+
+    let plugins : VvvvPlugin array =
+      [| for n in 0 .. (rand.Next(2,8)) do
+          yield { Name = rndstr()
+                  Path = rndstr() } |]
+
+    { Executables = exes
+      Plugins = plugins }
+
+  let mkHostGroup () : HostGroup =
+    let members =
+      [| for n in 0 .. (rand.Next(2,8)) do
+          yield Id.Create() |]
+    { Name = rndstr()
+      Members = members }
+
+  let mkCluster() =
+    let members =
+      mkMembers ()
+      |> Array.map (fun mem -> mem.Id,mem)
+      |> Map.ofArray
+
+    let groups =
+      [| for n in 0 .. (rand.Next(2,8)) do
+          yield mkHostGroup () |]
+
+    { Name = rndstr()
+      Members = members
+      Groups = groups }
+
+  let mkViewPort () =
+    { Id = Id.Create()
+      Name = rndstr()
+      Position = Coordinate(rand.Next(0,1280),rand.Next(0,1920))
+      Size = Rect(rand.Next(0,1280),rand.Next(0,1920))
+      OutputPosition = Coordinate(rand.Next(0,1280),rand.Next(0,1920))
+      OutputSize = Rect(rand.Next(0,1280),rand.Next(0,1920))
+      Overlap = Rect(rand.Next(0,1280),rand.Next(0,1920))
+      Description = rndstr() }
+
+  let mkViewPorts () =
+    [| for n in 0 .. (rand.Next(1,8)) do
+        yield mkViewPort() |]
+
+  let mkSignal () : Signal =
+    { Position = Coordinate(rand.Next(0,1280),rand.Next(0,1920))
+      Size = Rect(rand.Next(0,1280),rand.Next(0,1920))}
+
+  let mkSignals () =
+    [| for n in 0 .. (rand.Next(2,8)) do
+        yield mkSignal() |]
+
+  let mkRegion () =
+    { Id = Id.Create()
+      Name = rndstr()
+      SrcPosition = Coordinate(rand.Next(0,1280),rand.Next(0,1920))
+      SrcSize = Rect(rand.Next(0,1280),rand.Next(0,1920))
+      OutputPosition = Coordinate(rand.Next(0,1280),rand.Next(0,1920))
+      OutputSize = Rect(rand.Next(0,1280),rand.Next(0,1920))
+      }
+
+  let mkRegions () =
+    [| for n in 0 .. (rand.Next(2,8)) do
+        yield mkRegion() |]
+
+  let mkRegionMap () =
+    { SrcViewportId = Id.Create()
+      Regions = mkRegions() }
+
+  let mkDisplay () =
+    { Id = Id.Create()
+      Name = rndstr()
+      Size = Rect(rand.Next(0,1280),rand.Next(0,1920))
+      Signals = mkSignals()
+      RegionMap = mkRegionMap() }
+
+  let mkDisplays () =
+    [| for n in 0 .. (rand.Next(2,9)) do
+        yield mkDisplay() |]
+
+  let mkArguments () =
+    [| for n in 0 .. (rand.Next(2,8)) do
+        yield (rndstr(), rndstr()) |]
+
+  let mkTask () =
+    { Id = Id.Create()
+      Description = rndstr()
+      DisplayId = Id.Create()
+      AudioStream = rndstr()
+      Arguments = mkArguments() }
+
+  let mkTasks () =
+    [| for n in 0 .. (rand.Next(2,8)) do
+        yield mkTask() |]
+
+  let mkConfig () =
+    { Config.create (rndstr()) (mkMachine()) with
+        Vvvv = mkVvvv ()
+        Cluster = mkCluster()
+        Tasks = mkTasks()
+        ViewPorts = mkViewPorts()
+        Displays = mkDisplays () }
+
+  let mkProject () =
+    { Id        = Id.Create()
+    ; Name      = rndstr()
+    ; Path      = "C:/dev/null"
+    ; CreatedOn = Time.createTimestamp()
+    ; LastSaved = Some (Time.createTimestamp ())
+    ; Copyright = None
+    ; Author    = None
+    ; Config    = mkConfig ()  }
+
+  let mkClient () : IrisClient =
+    { Id = Id.Create ()
+      Name = "Nice client"
+      Role = Role.Renderer
+      Status = ServiceStatus.Running
+      IpAddress = IPv4Address "127.0.0.1"
+      Port = 8921us }
+
+  let mkClients () =
+    [| for n in 0 .. rand.Next(1,20) do
+        yield mkClient() |]
+
+  let inline asMap arr =
+    arr
+    |> Array.map toPair
+    |> Map.ofArray
+
+  let mkState path : State =
+    { Project  = mkProject ()
+      Patches  = mkPatches () |> asMap
+      Cues     = mkCues    () |> asMap
+      CueLists = mkCueLists() |> asMap
+      Sessions = mkSessions() |> asMap
+      Users    = mkUsers   () |> asMap
+      Clients  = mkClients () |> asMap }
+
+
 //  ____  _        _
 // / ___|| |_ __ _| |_ ___
 // \___ \| __/ _` | __/ _ \
@@ -59,10 +310,18 @@ type StateNode() =
   [<Output("Update", IsSingle = true, IsBang = true)>]
   val mutable OutUpdate: ISpread<bool>
 
+  let mutable init = false
+  let mutable state = Unchecked.defaultof<State>
+
   interface IPluginEvaluate with
     member self.Evaluate (spreadMax: int) : unit =
-      if self.InUpdate.[0] && not (Util.isNull self.InState.[0]) then
-        let state = self.InState.[0]
+      if not init then
+        state <- mkState()
+        init <- true
+
+      if self.InUpdate.[0] (* && not (Util.isNull self.InState.[0]) *) then
+        // let state = self.InState.[0]
+        state <- mkState()
         self.OutProject.[0] <- state.Project
 
         let cues =
