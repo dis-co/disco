d52e8fda803022b5578799e249fc846ef9c44be6
Karsten Gebbert <k@ioctl.it>
2017-02-14 09:28:26 +0100

    reset Main to previous state

--- dotnet/src/Iris/Iris/Service/Main.fs
+++ dotnet/src/Iris/Iris/Service/Main.fs
@@ -12,16 +12,7 @@ open Iris.Service.CommandLine
 
 [<AutoOpen>]
 module Main =
-  ////////////////////////////////////////
-  //  __  __       _                    //
-  // |  \/  | __ _(_)_ __               //
-  // | |\/| |/ _` | | '_ \              //
-  // | |  | | (_| | | | | |             //
-  // |_|  |_|\__,_|_|_| |_|             //
-  ////////////////////////////////////////
-
-  [<EntryPoint>]
-  let main args =
+  // let altmain argv =
     //  ____        _       ______        _
     // |  _ \ _   _| |__   / / ___| _   _| |__
     // | |_) | | | | '_ \ / /\___ \| | | | '_ \
@@ -70,90 +61,93 @@ module Main =
     // /_/   \_\ .__/|_|\____|_|_|\___|_| |_|\__|   |_|\___||___/\__|
     //         |_|
 
-    let result =
-      either {
-        // let logobs = Logger.subscribe (string >> printfn "%s")
-
-        let pid = Id "hello-project-id"
-
-        let mem =
-          { Member.create (Id.Create()) with
-              IpAddr = IPv4Address args.[0]
-              ApiPort = 10000us }
-
-        let! server = ApiServer.create mem pid
-        do! server.Start()
-        do! server.SetState State.Empty
-        let obs = server.Subscribe(printfn "ApiEvent: %A")
-
-        return
-          { new IDisposable with
-              member self.Dispose() =
-                dispose obs
-                dispose server
-                (* dispose logobs *) }
-      }
-
-    match result with
-    | Right server ->
-      let mutable run = true
-
-      while run do
-        match Console.ReadLine() with
-        | "quit" | "exit" ->
-          dispose server
-          run <- false
-        | _ -> ()
-
-    | Left error ->
-      printfn "Error: %A" error
-
-    //  ____                  _
-    // |  _ \ ___  __ _ _   _| | __ _ _ __
-    // | |_) / _ \/ _` | | | | |/ _` | '__|
-    // |  _ <  __/ (_| | |_| | | (_| | |
-    // |_| \_\___|\__, |\__,_|_|\__,_|_|
-    //            |___/
-
-    // let parsed =
-    //   try
-    //     parser.ParseCommandLine args
-    //   with
-    //     | exn ->
-    //       exn.Message
-    //       |> Error.asOther "Main"
-    //       |> Error.exitWith
-
-    // validateOptions parsed
-
-    // // Init machine config
-    // parsed.TryGetResult <@ Machine @>
-    // |> Option.map System.IO.Path.GetFullPath
-    // |> MachineConfig.init
-    // |> Error.orExit ignore
-
     // let result =
-    //   let machine = MachineConfig.get()
-    //   let dir =
-    //     parsed.TryGetResult <@ Project @>
-    //     |> Option.map (fun projectName ->
-    //       machine.WorkSpace </> projectName)
+    //   either {
+    //     // let logobs = Logger.subscribe (string >> printfn "%s")
 
-    //   let interactive = parsed.Contains <@ Interactive @>
+    //     let pid = Id "hello-project-id"
 
-    //   match parsed.GetResult <@ Cmd @>, dir with
-    //   | Create,            _ -> createProject parsed
-    //   | Start,           dir -> startService interactive dir
-    //   | Reset,      Some dir -> resetProject dir
-    //   | Dump,       Some dir -> dumpDataDir dir
-    //   | Add_User,   Some dir -> addUser dir
-    //   | Add_Member, Some dir -> addMember dir
-    //   | Help,              _ -> help ()
-    //   |  _ ->
-    //     sprintf "Unexpected command line failure: %A" args
-    //     |> Error.asParseError "Main"
-    //     |> Either.fail
+    //     let mem =
+    //       { Member.create (Id.Create()) with
+    //           IpAddr = IPv4Address args.[0]
+    //           ApiPort = 10000us }
 
-    // result |> Error.orExit ignore
+    //     let! server = ApiServer.create mem pid
+    //     do! server.Start()
+    //     do! server.SetState State.Empty
+    //     let obs = server.Subscribe(printfn "ApiEvent: %A")
+
+    //     return
+    //       { new IDisposable with
+    //           member self.Dispose() =
+    //             dispose obs
+    //             dispose server
+    //             (* dispose logobs *) }
+    //   }
+
+    // match result with
+    // | Right server ->
+    //   let mutable run = true
+
+    //   while run do
+    //     match Console.ReadLine() with
+    //     | "quit" | "exit" ->
+    //       dispose server
+    //       run <- false
+    //     | _ -> ()
+
+    // | Left error ->
+    //   printfn "Error: %A" error
+
+  ////////////////////////////////////////
+  //  __  __       _                    //
+  // |  \/  | __ _(_)_ __               //
+  // | |\/| |/ _` | | '_ \              //
+  // | |  | | (_| | | | | |             //
+  // |_|  |_|\__,_|_|_| |_|             //
+  ////////////////////////////////////////
+
+  [<EntryPoint>]
+  let main args =
+    let parsed =
+      try
+        parser.ParseCommandLine args
+      with
+        | exn ->
+          exn.Message
+          |> Error.asOther "Main"
+          |> Error.exitWith
+
+    validateOptions parsed
+
+    // Init machine config
+    parsed.TryGetResult <@ Machine @>
+    |> Option.map System.IO.Path.GetFullPath
+    |> MachineConfig.init
+    |> Error.orExit ignore
+
+    let result =
+      let machine = MachineConfig.get()
+      let dir =
+        parsed.TryGetResult <@ Project @>
+        |> Option.map (fun projectName ->
+          machine.WorkSpace </> projectName)
+
+      let interactive = parsed.Contains <@ Interactive @>
+
+      match parsed.GetResult <@ Cmd @>, dir with
+      | Create,            _ -> createProject parsed
+      | Start,           dir -> startService interactive dir
+      | Reset,      Some dir -> resetProject dir
+      | Dump,       Some dir -> dumpDataDir dir
+      | Add_User,   Some dir -> addUser dir
+      | Add_Member, Some dir -> addMember dir
+      | Help,              _ -> help ()
+      |  _ ->
+        sprintf "Unexpected command line failure: %A" args
+        |> Error.asParseError "Main"
+        |> Either.fail
+
+    result |> Error.orExit ignore
 
     Error.exitWith IrisError.OK
