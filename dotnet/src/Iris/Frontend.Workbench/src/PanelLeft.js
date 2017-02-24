bc0c5981beb543d56f7c4a1364b6612a193c7e55
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2017-02-24 11:46:35 +0100

    Widget cards in left panel

--- dotnet/src/Iris/Frontend.Workbench/src/PanelLeft.js
+++ dotnet/src/Iris/Frontend.Workbench/src/PanelLeft.js
@@ -1,12 +1,16 @@
 import * as React from "react";
 import css from "../css/PanelLeft.less";
 
-const Square = props => (
-  <div className="iris-panel-left-child">
-    <div>P</div>
+function cardClicked(title) {
+  console.log(title);
+}
+
+const Card = props => (
+  <div className="iris-panel-left-child" onClick={() => cardClicked(props.title)}>
+    <div>{props.letter}</div>
     <div>
-      <p><strong>Project Overview (Big)</strong></p>
-      <p>Cluster Settings</p>
+      <p><strong>{props.title}</strong></p>
+      <p>{props.text}</p>
     </div>
   </div>
 );
@@ -19,9 +23,15 @@ export default class PanelLeft extends React.Component {
   render() {
     return (
       <div className="iris-panel-left">
-        <Square />
-        <Square />
-        <Square />
+        <Card key={0} letter="P" title="Project Overview (Big)" text="Cluster Settings" />
+        <Card key={1} letter="P" title="Project Overview (Small)" text="Cluster Settings" />
+        <Card key={2} letter="B" title="Branches" text="Cluster Settings" />
+        <Card key={3} letter="L" title="LOG" text="Cluster Settings" />
+        <Card key={4} letter="U" title="User Management" text="Cluster Settings" />
+        <Card key={5} letter="H" title="Unassigned Hosts" text="Cluster Settings" />
+        <Card key={6} letter="R" title="Remotter" text="Cluster Settings" />
+        <Card key={7} letter="S" title="Project Settings" text="Cluster Settings" />
+        <Card key={8} letter="L" title="Library" text="Graph View" />
       </div>
     )
   }
