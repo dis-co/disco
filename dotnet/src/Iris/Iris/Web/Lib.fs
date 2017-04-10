6bb56995a687c6347a85d0e4c182e122dd07ae7b
Alfonso García-Caro <alfonso.garcia-caro@hotmail.com>
2017-04-10 12:47:46 +0200

    Add notifications

--- dotnet/src/Iris/Iris/Web/Lib.fs
+++ dotnet/src/Iris/Iris/Web/Lib.fs
@@ -166,15 +166,19 @@ let rec loadProject(project, username, password, site) =
   |> postCommandPrivate
   |> Promise.bind (fun res ->
     if res.Ok
-    then ClientContext.Singleton.ConnectWithWebSocket() |> Promise.map nullify
-    else res.text() |> Promise.map (fun msg ->    
-      if msg.Contains(ErrorMessages.PROJECT_NO_ACTIVE_CONFIG)
-        || msg.Contains(ErrorMessages.PROJECT_MISSING_CLUSTER)
-        || msg.Contains(ErrorMessages.PROJECT_MISSING_MEMBER)
-      then msg
-      // We cannot deal with the error, just notify it
-      else notify msg |> nullify
-    )
+    then
+      ClientContext.Singleton.ConnectWithWebSocket()
+      |> Promise.map (fun _msg -> // TODO: Check message?
+        notify "The project has been loaded successfully" |> nullify)
+    else
+      res.text() |> Promise.map (fun msg ->    
+        if msg.Contains(ErrorMessages.PROJECT_NO_ACTIVE_CONFIG)
+          || msg.Contains(ErrorMessages.PROJECT_MISSING_CLUSTER)
+          || msg.Contains(ErrorMessages.PROJECT_MISSING_MEMBER)
+        then msg
+        // We cannot deal with the error, just notify it
+        else notify msg |> nullify
+      )
   )
 
 let getProjectSites(project, username, password) =
@@ -189,7 +193,7 @@ let createProject(info: obj) =
   ; webSocketPort = !!info?webSocketPort
   ; gitPort       = !!info?gitPort }
   |> CreateProject
-  |> postCommandAndForget
+  |> postCommand (fun _ -> notify "The project has been created successfully") notify
 
 let project2tree (p: IrisProject) =
   let leaf m = { ``module``=m; children=None }
