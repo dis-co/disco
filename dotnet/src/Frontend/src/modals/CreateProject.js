e3795ad3940b506531a67ce9831a73e2c67127d5
Alfonso García-Caro <alfonso.garcia-caro@hotmail.com>
2017-04-11 10:10:48 +0200

    Validate CreateProject modal

--- dotnet/src/Frontend/src/modals/CreateProject.js
+++ dotnet/src/Frontend/src/modals/CreateProject.js
@@ -1,36 +1,98 @@
-import React from 'react'
-import Form from 'muicss/lib/react/form'
-import Input from 'muicss/lib/react/input'
-import Button from 'muicss/lib/react/button'
-import css from 'muicss/dist/css/mui-noglobals.min.css'
-import { findParentTag } from '../Util.ts'
+import React, { Component } from 'react'
 
-export default function(props) {
-  return (
-    <Form>
-      <legend>Create Project</legend>
-      <Input name="project" label="Name" floatingLabel={true} required={true} />
-      <Input name="bind" label="IP Address" floatingLabel={true} required={true} />
-      <Input name="api" label="Api Port" floatingLabel={true} required={true} />
-      <Input name="raft" label="Raft Port" floatingLabel={true} required={true}  />
-      <Input name="ws" label="Web Socket Port" floatingLabel={true} required={true} />
-      <Input name="git" label="Git Daemon Port" floatingLabel={true} required={true}  />
-      <Button variant="raised"
-        onClick={ev => {
-          ev.preventDefault();
-          var form = findParentTag(ev.target, "form");
-          Iris.createProject({
-            name: form.project.value,
-            ipAddress: form.bind.value,
-            apiPort: form.api.value,
-            raftPort: form.raft.value,
-            webSocketPort: form.ws.value,
-            gitPort: form.git.value
-          });
-          props.onSubmit();
-        }}>
-        Submit
-      </Button>
-    </Form>
-  );
+export default class createProject extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        name: "",
+        nameError: "Required",
+        ipAddress: "",
+        ipAddressError: "Required",
+        apiPort: "",
+        apiPortError: "Required",
+        raftPort: "",
+        raftPortError: "Required",
+        webSocketPort: "",
+        webSocketPortError: "Required",
+        gitPort: "",
+        gitPortError: "Required"
+      };
+  }
+
+  isErrorName(id, name) {
+    return typeof name === "string" && name.length > 0 ? null : "Required";
+  }
+
+  isErrorAddress(id, address) {
+     return /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.test(address) ? null : "Not a valid IP address";
+  }
+
+  isErrorPort(id, port) {
+    // TODO: Check also range?
+    if (isNaN(parseInt(port))) {
+      return "Not a valid number";
+    }
+    for (let key in this.state) {
+      if (key !== id && key.indexOf("Port") > -1 && !key.endsWith("Error")) {
+        if (this.state[key] === port) {
+          return "Duplicated port"
+        }
+      }
+    }
+    return null;
+  }
+
+  renderGroup(id, label, isError, placeholder = "") {
+    const success = this.state[id + "Error"] == null;
+    return (
+      <div className="field">
+        <label className="label">{label}</label>
+        <p className="control has-icon has-icon-right">
+          <input className={"input " + (success ? "is-success" : "is-danger")}
+            type="text" placeholder={placeholder} value={this.state[id]} onChange={ev => {
+              const value = ev.target.value;
+              const error = isError(id, value);
+              this.setState({ [id]: value, [id + "Error"]: error})
+            }}/>
+          <span className="icon is-small">
+            <i className={"fa " + (success ? "fa-check" : "fa-warning")}></i>
+          </span>
+        </p>
+        {!success && <p className="help is-danger">{this.state[id + "Error"]}</p>}
+      </div>
+    );
+  }
+
+  render() {
+    const props = this.props;
+    let isValid = true;
+    for (let key in this.state) {
+      if (key.endsWith("Error") && this.state[key] != null) {
+        isValid = false;
+        break;
+      }
+    }
+    return (
+      <div>
+        <p className="title has-text-centered">Create Project</p>
+        {this.renderGroup("name", "Name", this.isErrorName.bind(this))}
+        {this.renderGroup("ipAddress", "IP Address", this.isErrorAddress.bind(this))}
+        {this.renderGroup("apiPort", "Api Port", this.isErrorPort.bind(this))}
+        {this.renderGroup("raftPort", "Raft Port", this.isErrorPort.bind(this))}
+        {this.renderGroup("webSocketPort", "Web Socket Port", this.isErrorPort.bind(this))}
+        {this.renderGroup("gitPort", "Git Daemon Port", this.isErrorPort.bind(this))}
+        <div className="field is-grouped">
+          <p className="control">
+            <button className="button is-primary" disabled={!isValid} onClick={ev => {
+              ev.preventDefault();
+              Iris.createProject(this.state);
+              props.onSubmit();
+            }}>
+              Submit
+            </button>
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
