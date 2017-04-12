5af6af5dc74fd32ae9eeb2479b16f618da8a18dd
Alfonso García-Caro <alfonso.garcia-caro@hotmail.com>
2017-04-12 12:24:06 +0200

    Update project config dialog

--- dotnet/src/Frontend/src/modals/ProjectConfig.js
+++ dotnet/src/Frontend/src/modals/ProjectConfig.js
@@ -1,10 +1,4 @@
 import React, { Component } from 'react'
-import Form from 'muicss/lib/react/form'
-import Input from 'muicss/lib/react/input'
-import Button from 'muicss/lib/react/button'
-import Dropdown from 'muicss/lib/react/dropdown';
-import DropdownItem from 'muicss/lib/react/dropdown-item';
-import { findParentTag } from '../Util.ts'
 
 export default class ProjectConfig extends Component {
   constructor(props) {
@@ -14,29 +8,44 @@ export default class ProjectConfig extends Component {
 
   render() {
     return (
-      <Form>
-        <legend>Project Config</legend>
+      <div>
+        <p className="title has-text-centered">Project Config</p>
         <p>
           Select a cluster in the project configuration or type a name to create a new one.
           The current machine will be added to the selected cluster if not present.
         </p>
-        <div>
-          <Dropdown color="primary" name="select" label="Sites">
-            {this.props.sites.map((site,i) =>
-              <DropdownItem key={i} onClick={ev => this.setState({selected: site})}>{site}</DropdownItem>)}
-          </Dropdown>
-          <Input style={{
+        <div className="field">
+          <label className="label">Sites</label>
+          <p className="control">
+            <span className="select">
+              <select>
+                {this.props.sites.map((site,i) =>
+                  <option key={i} onClick={ev => this.setState({selected: site})}>{site}</option>)}
+              </select>
+            </span>
+          </p>
+        </div>
+          {/*<Input style={{
             display: "inline-block",
             marginLeft: 20
-          }} label="Site" floatingLabel={true} required={false} value={this.state.selected} onChange={ev => this.setState({selected: ev.target.value})} />
+          }} label="Site" floatingLabel={true} required={false} value={this.state.selected} onChange={ev => this.setState({selected: ev.target.value})} />*/}
+        <div className="field">
+          <label className="label">Site</label>
+          <p className="control">
+            <input className="input" type="text" value={this.state.selected} onChange={ev => this.setState({selected: ev.target.value})}/>
+          </p>
+        </div>        
+        <div className="field">
+          <p className="control">
+            <button className="button is-primary"  disabled={this.state.selected == null} onClick={ev => {
+              ev.preventDefault();
+              this.props.onSubmit(this.state.selected);
+            }}>
+              Submit
+            </button>
+          </p>
         </div>
-        <Button variant="raised" enabled={!!this.state.selected} onClick={ev => {
-            ev.preventDefault();
-            this.props.onSubmit(this.state.selected);
-          }}>
-          Select
-        </Button>
-      </Form>
+      </div>
     );
   }
 }
