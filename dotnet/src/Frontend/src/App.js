7c631ad448495c67819688d741c999bfb2d4408e
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2017-06-05 18:54:27 +0200

    Fix modal dialog and dropdown in Linux

--- dotnet/src/Frontend/src/App.js
+++ dotnet/src/Frontend/src/App.js
@@ -4,35 +4,15 @@ import "react-grid-layout/css/styles.css"
 
 import values from "./values"
 import { GlobalModel } from "../fable/Frontend/GlobalModel.fs"
-import * as util from "./Util"
 
 import React, { Component } from 'react'
 import PanelLeft from './PanelLeft'
 import Tabs from './Tabs'
 import Workspace from './Workspace'
-import DropdownMenu from './DropdownMenu'
-import ModalDialog from './modals/ModalDialog'
-import CreateProject from './modals/CreateProject'
-import SimpleModal from './modals/Simple'
-
-let modal = null;
+import NavBar, { showModal as showModalNavBar } from './widgets/NavBar'
 
 export function showModal(content, props) {
-  props = props != null ? props : {};
-  return new Promise(resolve =>
-    modal.setState({ content, props, onSubmit: resolve })
-  );
-}
-
-export function askModal(title, text, buttons) {
-  buttons = buttons || [["Yes", true], ["No", false]];
-  return new Promise((resolve, reject) => {
-    modal.setState({
-      content: SimpleModal,
-      props: { title, text, buttons },
-      onSubmit: resolve
-    })
-  });
+  return showModalNavBar(content, props);
 }
 
 export default class App extends Component {
@@ -44,10 +24,6 @@ export default class App extends Component {
       view: Workspace,
       isFixed: true
     });
-    this.global.subscribe("serviceInfo", serviceInfo => this.setState({serviceInfo}));
-    this.state = {
-      serviceInfo: this.global.state.serviceInfo
-    };
   }
 
   componentDidMount() {
@@ -62,37 +38,9 @@ export default class App extends Component {
   }
 
   render() {
-    var global = this.global;
     return (
       <div id="app">
-        <ModalDialog ref={el => modal = (el || modal)} />
-        <header id="app-header">
-          <h1>Iris</h1>
-          <DropdownMenu options={[
-            {
-              label: () => "Create project",
-              action: () => showModal(CreateProject),
-            },
-            {
-              label: () => "Load project",
-              action: () => util.loadProject(),
-            },
-            {
-              label: () => "Unload project",
-              action: () => IrisLib.unloadProject(),
-            },
-            {
-              label: () => "Shutdown",
-              action: () => IrisLib.shutdown(),
-            },
-            {
-              label: () => "Use right click: " + global.state.useRightClick,
-              action: () => global.useRightClick(!global.state.useRightClick),
-            }
-          ]} />
-          <div className="separator" />
-          <span>Iris v{this.state.serviceInfo.version} - build {this.state.serviceInfo.buildNumber}</span>
-        </header>
+        <NavBar global={this.global} />
         <div id="app-content">
           <div id="ui-layout-container">
             <div className="ui-layout-west">
---
+++ dotnet/src/Frontend/src/widgets/NavBar.js
@@ -0,0 +1,123 @@
+import * as React from "react";
+import SkyLight from 'react-skylight';
+import * as util from "../Util";
+import CreateProject from '../modals/CreateProject';
+
+let navBarInstance = null;
+
+export function showModal(content, props) {
+  if (navBarInstance == null) {
+      throw new Error("No NavBar instance available");
+  }
+  return navBarInstance.showModal(content, props);
+}
+
+export default class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        modal: null,
+        isDropdownOpen: false,
+        serviceInfo: props.global.state.serviceInfo,
+        useRightClick: props.global.state.useRightClick
+    };
+  }
+
+  componentDidMount() {
+    navBarInstance = this;
+    this.disposable =
+        this.props.global.subscribe("serviceInfo", serviceInfo =>
+            this.setState({serviceInfo}));
+  }
+
+  componentWillUnmount() {
+    navBarInstance = null;
+    if (this.disposable) {
+      this.disposable.dispose();
+    }
+  }
+
+  onClick(dropdownIndex) {
+    return () => {
+      switch (dropdownIndex) {
+          case 0:
+            showModal(CreateProject);
+            break;
+          case 1:
+            util.loadProject();
+            break;
+          case 2:
+            IrisLib.unloadProject();
+            break;
+          case 3:
+            IrisLib.shutdown();
+            break;
+          case 4:
+            var useRightClick = !this.state.useRightClick;
+            this.props.global.useRightClick(useRightClick);
+            this.setState({useRightClick});
+            break;
+      }
+    }
+  }
+
+  showModal(content, props) {
+    return new Promise(resolve =>
+        this.setState({
+            modal: {
+                content,
+                props: props || {},
+                onSubmit: resolve
+            }
+        }, () => this.modalEl.show())
+    );
+  }
+
+  renderModal() {
+    if (this.state.modal == null) {
+        return null;
+    }
+
+    return React.createElement(this.state.modal.content, {
+      onSubmit: values => {
+        this.modalEl.hide();
+        this.state.modal.onSubmit(values);
+      },
+      ...this.state.modal.props
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SkyLight
+            hideOnOverlayClicked
+            ref={el => this.modalEl = el}
+            title={this.state.modal ? this.state.modal.title : ""}
+            dialogStyles={{height: "inherit"}}>
+            {this.renderModal()}
+        </SkyLight>
+        <nav id="app-header" className="nav">
+            <div className="nav-left">
+                <h1 className="nav-item">Iris</h1>
+                <a className="nav-item" onClick={() => this.setState({isDropdownOpen: !this.state.isDropdownOpen})}>Menu</a>
+                {this.state.isDropdownOpen ?
+                    <div className="box dropdown">
+                    <ul>
+                        <li><a onClick={this.onClick(0).bind(this)}>Create Project</a></li>
+                        <li><a onClick={this.onClick(1).bind(this)}>Load Project</a></li>
+                        <li><a onClick={this.onClick(2).bind(this)}>Unload Project</a></li>
+                        <li><a onClick={this.onClick(3).bind(this)}>Shutdown</a></li>
+                        <li><a onClick={this.onClick(4).bind(this)}>{"Use right click: " + this.state.useRightClick}</a></li>
+                    </ul>
+                    </div>
+                    : null}
+            </div>
+            <div className="nav-right">
+                <span className="nav-item">Iris v{this.state.serviceInfo.version} - build {this.state.serviceInfo.buildNumber}</span>
+            </div>
+        </nav>
+      </div>
+    )
+  }
+}
