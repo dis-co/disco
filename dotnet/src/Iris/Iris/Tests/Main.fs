0c8f377ee4d07c8a4bdadc1b27c7a9aecafd9de6
Karsten Gebbert <k@ioctl.it>
2017-08-24 15:15:38 +0200

    better structure in Tests main

--- dotnet/src/Iris/Iris/Tests/Main.fs
+++ dotnet/src/Iris/Iris/Tests/Main.fs
@@ -7,6 +7,47 @@ open Expecto.Impl
 open Iris.Core
 open Iris.Tests
 
+let setupVM () =
+  Thread.CurrentThread.GetApartmentState()
+  |> printfn "threading model: %A"
+
+  let threadCount = System.Environment.ProcessorCount * 8
+  ThreadPool.SetMinThreads(threadCount,threadCount)
+  |> printfn "set min threads %b"
+
+  ThreadPool.GetMinThreads()
+  |> printfn "min threads (worker,io): %A"
+
+let expectoConfig =
+  { defaultConfig with
+      printer =
+        { TestPrinters.defaultPrinter with
+            passed = fun name ts -> async {
+                Console.white      "{0}"     "["
+                Console.green      "{0}"     "OK"
+                Console.white      "{0}"     "]"
+                Console.white      "{0}"     " "
+                Console.white      "{0}"     name
+                Console.white      "{0}"     " "
+                Console.darkYellow "({0}ms)" ts.Milliseconds
+                Console.Write System.Environment.NewLine
+              }
+
+            failed = fun name msg ts -> async {
+                Console.white      "{0}"     "["
+                Console.red        "{0}"     "ERROR"
+                Console.white      "{0}"     "]"
+                Console.white      "{0}"     " "
+                Console.red        "{0}"     name
+                Console.white      "{0}"     " "
+                Console.white      "{0}"     msg
+                Console.white      "{0}"     " "
+                Console.darkYellow "({0}ms)" ts.Milliseconds
+                Console.Write System.Environment.NewLine
+              }
+          }
+      }
+
 let parallelTests =
   testList "parallel tests" [
       utilTests
@@ -38,49 +79,9 @@ let all =
 
 [<EntryPoint>]
 let main _ =
-
   // Tracing.enable()
   use lobs = Logger.subscribe (Logger.filter Trace Logger.stdout)
+  do Logger.initialize LoggingSettings.defaultSettings
+  do setupVM()
 
-  Logger.initialize LoggingSettings.defaultSettings
-
-  Thread.CurrentThread.GetApartmentState()
-  |> printfn "threading model: %A"
-
-  let threadCount = System.Environment.ProcessorCount * 8
-  ThreadPool.SetMinThreads(threadCount,threadCount)
-  |> printfn "set min threads %b"
-
-  ThreadPool.GetMinThreads()
-  |> printfn "min threads (worker,io): %A"
-
-
-  let printers =
-    { TestPrinters.defaultPrinter with
-        passed = fun name ts -> async {
-            Console.white      "{0}"     "["
-            Console.green      "{0}"     "OK"
-            Console.white      "{0}"     "]"
-            Console.white      "{0}"     " "
-            Console.white      "{0}"     name
-            Console.white      "{0}"     " "
-            Console.darkYellow "({0}ms)" ts.Milliseconds
-            Console.Write System.Environment.NewLine
-          }
-
-        failed = fun name msg ts -> async {
-            Console.white      "{0}"     "["
-            Console.red        "{0}"     "ERROR"
-            Console.white      "{0}"     "]"
-            Console.white      "{0}"     " "
-            Console.red        "{0}"     name
-            Console.white      "{0}"     " "
-            Console.white      "{0}"     msg
-            Console.white      "{0}"     " "
-            Console.darkYellow "({0}ms)" ts.Milliseconds
-            Console.Write System.Environment.NewLine
-          }
-      }
-
-  let config = { defaultConfig with printer = printers }
-  runTests config all
+  runTests expectoConfig all
