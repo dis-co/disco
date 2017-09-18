5325872f2e133a3add5451c9c8e24baea5e89a64
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2017-09-18 19:04:38 +0200

    Detect dragging on PinMappingView

--- dotnet/src/Frontend/src/Frontend/Elmish/PinMappingView.fs
+++ dotnet/src/Frontend/src/Frontend/Elmish/PinMappingView.fs
@@ -16,6 +16,8 @@ open Helpers
 open State
 open Types
 
+let touchesElement(el: Browser.Element option, x: float, y: float): bool = importMember "../../js/Util"
+
 let inline padding5() =
   Style [PaddingLeft "5px"]
 
@@ -34,33 +36,69 @@ let renderPin (model: Model) (pin: Pin) =
       updater = None
       onDragStart = None } []
 
-let addPinRow i =
-  tr [
-    Key (sprintf "ADD_MAPPING%i" i)
-    Class "iris-add-pinmapping"
-  ] [
-    td [Class "width-20"; Style [PaddingLeft "5px"]] []
-    td [Class "width-75"] []
-    td [Class "width-5"] [
-      button [
-        // TODO: Enable button only if source and at least one sink are set
-        Class "iris-button iris-icon icon-more"
-        OnClick (fun ev ->
-          ev.stopPropagation()
-          printfn "TODO: Add PinMapping")
-      ] []
-    ]
-  ]
+type [<Pojo>] PinHoleProps =
+  { Classes: string list
+    Padding: bool
+    AddPin: Pin -> unit
+    Render: unit -> ReactElement }
+
+type [<Pojo>] PinHoleState =
+  { IsHighlit: bool }
+
+type PinHole(props) =
+  inherit Component<PinHoleProps, PinHoleState>(props)
+  let mutable selfRef: Browser.Element option = None
+  let mutable disposable: IDisposable option = None
+  do base.setInitState({ IsHighlit = false })
+
+  member this.componentWillUnmount() =
+    disposable |> Option.iter (fun disp -> disp.Dispose())
+
+  member this.componentDidMount() =
+    disposable <-
+      Drag.observe()
+      |> Observable.choose(function
+        | Drag.Moved(x,y,Drag.Pin pin) -> Some(pin,x,y,false)
+        | Drag.Stopped(x,y,Drag.Pin pin) -> Some(pin,x,y,true))
+      |> Observable.subscribe(fun (pin,x,y,stopped) ->
+        let isHighlit  =
+          if touchesElement(selfRef, x, y) then
+            if not stopped then
+              true
+            else
+              this.props.AddPin(pin)
+              false
+          else
+            false
+        if isHighlit <> this.state.IsHighlit then
+          this.setState({ IsHighlit = isHighlit })
+      ) |> Some
+
+  member this.render() =
+    let isHighlit = this.state.IsHighlit
+    let classes =
+      [ for c in this.props.Classes do
+          yield c, true
+        yield "iris-highlight", isHighlit
+        yield "iris-blue", isHighlit]
+    td [classList classes
+        Style [PaddingLeft (if this.props.Padding then "5px" else "0")
+               Border "2px solid transparent"]
+        Ref (fun el -> selfRef <- Option.ofObj el)]
+       [this.props.Render()]
 
 type [<Pojo>] PinMappingProps =
-  { Model: Model }
+  { Id: Guid
+    Name: string
+    Model: Model
+    Dispatch: Msg -> unit }
 
 type [<Pojo>] PinMappingState =
   { SourceCandidate: Pin option
     SinkCandidates: Pin list }
 
 type PinMappingView(props) =
-  inherit React.Component<PinMappingProps, PinMappingState>(props)
+  inherit Component<PinMappingProps, PinMappingState>(props)
   do base.setInitState({ SourceCandidate = None; SinkCandidates = [] })
 
   member this.shouldComponentUpdate(nextProps, nextState, nextContext) =
@@ -70,7 +108,36 @@ type PinMappingView(props) =
     | None, None -> false
     | _ -> true
 
-  member this.render() =
+  member this.renderLastRow() =
+    let disabled =
+      Option.isNone this.state.SourceCandidate
+        || List.isEmpty this.state.SinkCandidates
+    tr [
+      Key "iris-add-pinmapping"
+      Class "iris-add-pinmapping"
+    ] [
+      com<PinHole,_,_>
+        { Classes = ["width-20"]
+          Padding = true
+          AddPin = fun pin -> failwith "Add pin"
+          Render = fun () -> div [] [] } []
+      com<PinHole,_,_>
+        { Classes = ["width-75"]
+          Padding = true
+          AddPin = fun pin -> failwith "Add pin"
+          Render = fun () -> div [] [] } []
+      td [Class "width-5"] [
+        button [
+          Class "iris-button iris-icon icon-more"
+          Disabled disabled
+          OnClick (fun ev ->
+            ev.stopPropagation()
+            printfn "TODO: Add PinMapping")
+        ] []
+      ]
+    ]
+
+  member this.renderBody() =
     let model = this.props.Model
     table [Class "iris-table"] [
       thead [] [
@@ -103,10 +170,15 @@ type PinMappingView(props) =
                 ] []
               ]
             ]
-          yield addPinRow 1
+          yield this.renderLastRow()
         | None -> ()
         ]
       ]
+
+  member this.render() =
+    widget this.props.Id this.props.Name
+      None
+      (fun _ _ -> this.renderBody()) this.props.Dispatch this.props.Model
 
 let createWidget(id: System.Guid) =
   { new IWidget with
@@ -119,4 +191,9 @@ let createWidget(id: System.Guid) =
         minW = 4; maxW = 10
         minH = 1; maxH = 10 }
     member this.Render(dispatch, model) =
-      com<PinMappingView,_,_> { Model = model } []  }
+      com<PinMappingView,_,_>
+        { Id = this.Id
+          Name = this.Name
+          Model = model
+          Dispatch = dispatch } []
+  }
