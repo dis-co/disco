e54d0160a673efa1c711b8d4d4bd6dfbeb030cf2
Karsten Gebbert <k@ioctl.it>
2017-09-21 16:17:05 +0200

    fleshing out group inspector

--- dotnet/src/Frontend/src/Frontend/Elmish/Inspectors/PinGroupInspector.fs
+++ dotnet/src/Frontend/src/Frontend/Elmish/Inspectors/PinGroupInspector.fs
@@ -11,6 +11,7 @@ open Fable.Core.JsInterop
 open Fable.PowerPack
 open Elmish.React
 open Iris.Core
+open Iris.Web.PinView
 open Iris.Web.Core
 open Iris.Web.Helpers
 open Iris.Web.Types
@@ -23,20 +24,57 @@ open State
 /// |_|    \__,_|_.__/|_|_|\___|
 
 module PinGroupInspector =
+  let private buildPin dispatch (model: Model) (pinId, pin) =
+    li [] [
+      com<PinView,_,_> {
+        key = string pinId
+        pin = pin
+        useRightClick = model.userConfig.useRightClick
+        slices = None
+        updater = None
+        onSelect = fun () -> Select.pin dispatch pin
+        onDragStart = None
+      } []
+    ]
+
   let private renderPins (tag: string) dispatch (model: Model) (group: PinGroup) =
-    tr [] [
-      td [] [ str "pins go here..." ]
+    let pins =
+      group.Pins
+      |> Map.toList
+      |> List.map (buildPin dispatch model)
+
+    Common.row tag [
+      ul [ Class "iris-graphview" ] pins
     ]
 
   let private renderClients tag dispatch model group =
-    tr [] [
-      td [] [ str "pins go here..." ]
-    ]
+    match model.state with
+    | None -> []
+    | Some state ->
+      state.PinGroups
+      |> PinGroupMap.findGroupBy (fun g -> g.Id = group.Id)
+      |> Map.toList
+      |> List.map
+        (fun (clientId,group) ->
+          match Map.tryFind clientId state.Clients with
+          | Some client -> li [] [ str (string client.Name) ]
+          | None ->
+            match ClientConfig.tryFind clientId state.Project.Config.Clients with
+            | Some exe -> li [] [ str (string exe.Id) ]
+            | None -> li [] [ str (string clientId + " (orphaned)") ])
+      |> fun clients -> [ ul [] clients ]
+      |> Common.row tag
+
+  let private renderRefersTo tag dispatch (model: Model) (group: PinGroup) =
+    tr [] []
 
   let render dispatch (model: Model) (group: PinGroup) =
     Common.render "Pin Group" [
-      Common.stringRow "Id"      (string group.Id)
-      Common.stringRow "Name"    (string group.Name)
-      renderClients    "Clients"  dispatch model group
-      renderPins       "Pins"     dispatch model group
+      Common.stringRow "Id"         (string group.Id)
+      Common.stringRow "Name"       (string group.Name)
+      Common.stringRow "Path"       (string group.Path)
+      Common.stringRow "Asset Path" (string group.AssetPath)
+      renderRefersTo   "Belongs To"  dispatch model group
+      renderClients    "Clients"     dispatch model group
+      renderPins       "Pins"        dispatch model group
     ]
