a92ff7dd832ad3c138c7d674748278b6791a872d
Karsten Gebbert <k@ioctl.it>
2017-09-26 11:03:34 +0200

    non-persisted pins should have blue outline

--- dotnet/src/Frontend/src/Frontend/Elmish/PinView.fs
+++ dotnet/src/Frontend/src/Frontend/Elmish/PinView.fs
@@ -113,7 +113,7 @@ type PinView(props) =
     let classes =
       ["iris-pin", true
        "iris-dirty", pin.Dirty
-       "iris-persisted", pin.Persisted && pin.Online
+       "iris-persisted", not pin.Persisted && pin.Online
        "iris-offline", pin.Persisted && not pin.Online
        ]
     div [classList classes] [
