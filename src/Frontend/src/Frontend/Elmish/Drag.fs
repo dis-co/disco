035ae8a12087c2b75bb9d496a3f3a9b64b898df4
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2017-10-12 12:07:25 +0200

    Use an abstract symbol to represent drag elements

--- src/Frontend/src/Frontend/Elmish/Drag.fs
+++ src/Frontend/src/Frontend/Elmish/Drag.fs
@@ -21,6 +21,9 @@ let jQueryEventAsPromise(selector:obj, events:string): JS.Promise<obj> = importM
 
 type Data =
   | Pin of Pin list
+  member this.Length =
+    match this with
+    | Pin pins -> List.length pins
 
 type Event =
   | Moved of x:float * y:float * data:Data
@@ -41,31 +44,22 @@ let private trigger (ev: Event) =
     obs.OnNext(ev)
 
 let start el (data:Data) =
-  Promise.race
-    (domtoimage.toPng(el, obj()))
-    (jQueryEventAsPromise(Browser.document, "mouseup.domtoimage"))
-  |> Promise.iterOrError
-    (function
-      | Choice1Of2 dataUrl ->
-        let mutable prev: Point option = None
-        let img = jQuery("#iris-drag-image")
-        !!jQuery(Browser.window.document)
-          ?on("mousemove.drag", fun e ->
-            let cur = { Point.x = !!e?clientX; y = !!e?clientY }
-            match prev with
-            | None ->
-              prev <- Some cur
-              !!jQuery(img)?attr("src", dataUrl)?css(%["display" => "block"; "left" => cur.x; "top" => cur.y])
-              Moved(cur.x, cur.y, data) |> trigger
-            | Some p when distance p cur > 5. ->
-              prev <- Some cur
-              !!jQuery(img)?css(%["left" => cur.x; "top" => cur.y])
-              Moved(cur.x, cur.y, data) |> trigger
-            | Some _ -> ())
-          ?on("mouseup.drag", fun e ->
-            !!jQuery(img)?css(%["display" => "none"])
-            Stopped(!!e?clientX, !!e?clientY, data) |> trigger
-            jQuery(Browser.window.document)?off("mousemove.drag mouseup.drag"))
-      // If the mouseup event happens before the image is finished, do nothing
-      | Choice2Of2 _ -> ())
-    (fun ex -> Browser.console.error("Error when generating image:", ex))
+  let mutable prev: Point option = None
+  let img = jQuery("#iris-drag-image")
+  !!jQuery(Browser.window.document)
+    ?on("mousemove.drag", fun e ->
+      let cur = { Point.x = !!e?clientX; y = !!e?clientY }
+      match prev with
+      | None ->
+        prev <- Some cur
+        !!jQuery(img)?text(data.Length)?css(%["display" => "flex"; "left" => cur.x; "top" => cur.y])
+        Moved(cur.x, cur.y, data) |> trigger
+      | Some p when distance p cur > 5. ->
+        prev <- Some cur
+        !!jQuery(img)?css(%["left" => cur.x; "top" => cur.y])
+        Moved(cur.x, cur.y, data) |> trigger
+      | Some _ -> ())
+    ?on("mouseup.drag", fun e ->
+      !!jQuery(img)?css(%["display" => "none"])
+      Stopped(!!e?clientX, !!e?clientY, data) |> trigger
+      jQuery(Browser.window.document)?off("mousemove.drag mouseup.drag"))
