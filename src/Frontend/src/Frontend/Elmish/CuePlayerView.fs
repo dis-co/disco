98307021561129ef1e7ab58c77e95129683f438d
Karsten Gebbert <k@ioctl.it>
2017-10-19 15:24:10 +0200

    set pin as persisted (unless it already is) when added to a cue

--- src/Frontend/src/Frontend/Elmish/CuePlayerView.fs
+++ src/Frontend/src/Frontend/Elmish/CuePlayerView.fs
@@ -110,20 +110,27 @@ type private CueView(props) =
               true, this.state.IsOpen
             else
               // Filter out output pins and pins already contained by the cue
-              let sliceses =
-                pins |> Seq.choose (fun pin ->
-                  if isOutputPin pin then
-                    None
-                  else
-                    let id = pin.Id
-                    let existing = this.props.Cue.Slices |> Array.exists (fun slices -> slices.PinId = id)
-                    if existing then
-                      printfn "The cue already contains pin %O" id
-                      None
-                    else Some pin.Slices)
-                |> Seq.toArray
-              let newCue = { this.props.Cue with Slices = Array.append this.props.Cue.Slices sliceses }
-              UpdateCue newCue |> ClientContext.Singleton.Post
+              let persistPins, updatedCue =
+                Seq.fold
+                  (fun (persistedPins, cue) pin ->
+                    if isOutputPin pin || Cue.contains pin.Id this.props.Cue
+                    then persistedPins, cue
+                    else
+                      let cue = Cue.addSlices pin.Slices cue
+                      match pin.Persisted with
+                      | true  -> persistedPins, cue    /// the pin already is persisted, do nothing
+                      | false -> pin :: persistedPins,cue)
+                  (List.empty, this.props.Cue)
+                  pins
+
+              let cueUpdate = UpdateCue updatedCue
+              if List.isEmpty persistPins then
+                ClientContext.Singleton.Post cueUpdate
+              else
+                let pinUpdates = List.map (Pin.setPersisted true >> UpdatePin) persistPins
+                cueUpdate :: pinUpdates
+                |> CommandBatch.ofList
+                |> ClientContext.Singleton.Post
               false, true
           else
             false, this.state.IsOpen
