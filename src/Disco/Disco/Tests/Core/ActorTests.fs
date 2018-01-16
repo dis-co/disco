e4dc6f0698ac92208e5f3e46ddb2afe14d6dc651
Karsten Gebbert <k@ioctl.it>
2018-01-16 18:36:46 +0100

    adding performance test for actor systems

--- src/Disco/Disco/Tests/Core/ActorTests.fs
+++ src/Disco/Disco/Tests/Core/ActorTests.fs
@@ -9,6 +9,7 @@ namespace Disco.Tests
 
 open System
 open System.Threading
+open System.Diagnostics
 open System.Collections.Concurrent
 open Expecto
 open FsCheck
@@ -21,19 +22,57 @@ open Microsoft.FSharp.Control
 [<AutoOpen>]
 module ActorTests =
 
-  let test_actor_properly =
-    testCase "actor disposes properly" <| fun _ ->
-      either {
-        let actor = ThreadActor.create "test" <| fun actor -> function
-          | "done" -> dispose actor
-          | msg -> ignore msg
-        do actor.Start()
-        do ignore [ for n in 0 .. 9 -> actor.Post (string n) ]
-        do actor.Post "done"
-      }
-      |> noError
+  [<Literal>]
+  let NUM = 10000
+
+  type Measurement = unit -> unit
+  type Measurements = ConcurrentQueue<int64>
+
+  let measurement (measurements:Measurements) =
+    let watch = Stopwatch()
+    watch.Start()
+    fun () ->
+      watch.Stop()
+      measurements.Enqueue watch.ElapsedMilliseconds
+
+  let measure (measurements:Measurements) (actor:IActor<Measurement>) =
+    measurements
+    |> measurement
+    |> actor.Post
+
+  let asyncLoop _ msg = async { msg() }
+  let threadLoop _ msg = msg()
+
+  let medianTime (arr: int64 array) =
+    let sum = Array.fold (+) 0L arr
+    decimal sum / (decimal arr.Length)
+
+  let analyse tag (measurements:Measurements) =
+    measurements.ToArray()
+    |> medianTime
+    |> printfn "median %s latency in ms: %f" tag
+
+  let test_actor_performance =
+    testCase "actor performance" <| fun _ ->
+      let asyncMeasurements = Measurements()
+      let threadMeasurements = Measurements()
+
+      let asyncActor = AsyncActor.create "async" asyncLoop
+      let threadActor = ThreadActor.create "thread" threadLoop
+
+      threadActor.Start()
+      asyncActor.Start()
+
+      ignore [ for n in 1 .. NUM -> measure asyncMeasurements asyncActor ]
+      while asyncMeasurements.Count <> NUM do Thread.Sleep(1)
+
+      ignore [ for n in 1 .. NUM -> measure threadMeasurements threadActor ]
+      while threadMeasurements.Count <> NUM do Thread.Sleep(1)
+
+      analyse "async"  asyncMeasurements
+      analyse "thread" threadMeasurements
 
   let actorTests =
-    testList "Actor Tests" [
-      test_actor_properly
+    ftestList "Actor Tests" [
+      test_actor_performance
     ]
